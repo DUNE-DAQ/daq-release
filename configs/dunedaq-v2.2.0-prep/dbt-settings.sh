d56c565a710903a22125a930b922c8ea755bcc87
Pengfei Ding <dingpf@fnal.gov>
2021-01-19 15:33:27 -0600

    upgrading cetlib and boost versions, to fix sqlite version conflict

--- configs/dunedaq-v2.2.0-prep/dbt-settings.sh
+++ configs/dunedaq-v2.2.0-prep/dbt-settings.sh
@@ -2,27 +2,21 @@ dune_products_dirs=(
     # "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-v2.2.0/externals"
     # "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-v2.2.0/packages"
     "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products" 
-    "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products_dev" 
+    #"/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products_dev" 
 )
 
 dune_products=(
     "cmake v3_17_2"
+    "cetlib v3_11_01 e19:prof"
     "gdb v9_2"
-    "gcc v8_2_0 e19:prof"
-    "boost v1_70_0 e19:prof"
-    "cetlib v3_10_00 e19:prof"
     "TRACE v3_16_02"
-    "folly v2020_05_25 e19:prof"
-    "ers v0_26_00c e19:prof"
-    "nlohmann_json v3_9_0b e19:prof"
+    "folly v2020_05_25a e19:prof"
+    "ers v0_26_00d e19:prof"
+    "nlohmann_json v3_9_0c e19:prof"
     "ninja v1_10_0"
     "pistache v2020_10_07 e19:prof"
-    "highfive v2_2_2 e19:prof"
-
     "cmdlib v1_0_2 e19:prof"
     "restcmd v1_0_2 e19:prof"
-    # Note: "daq_cmake" with underscore is the UPS product name.
-    # One can use either "daq-cmake" or "daq_cmake" in this file.
     "daq-cmake v1_2_1 e19:prof"
     "appfwk v2_0_1 e19:prof"
     "listrev v2_0_1 e19:prof"
