bcad08fa9171c686f40a6faad8c74905b54e7987
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2021-02-04 15:30:39 -0600

    adding configs for development release

---
+++ configs/dunedaq-develop/dbt-settings.sh
@@ -0,0 +1,22 @@
+dune_products_dirs=(
+    "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-develop/externals"
+    "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-develop/packages"
+    #"/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products" 
+    #"/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products_dev" 
+)
+
+dune_products=(
+    "cmake v3_17_2"
+    "cetlib v3_11_01 e19:prof"
+    "gdb v9_2"
+    "TRACE v3_16_02"
+    "folly v2020_05_25a e19:prof"
+    "ers v0_26_00d e19:prof"
+    "nlohmann_json v3_9_0c e19:prof"
+    "ninja v1_10_0"
+    "pistache v2020_10_07 e19:prof"
+    "highfive v2_2_2b e19:prof"
+    "zmq v4_3_1b e19"
+)
+
+dune_python_version="v3_8_3b"
