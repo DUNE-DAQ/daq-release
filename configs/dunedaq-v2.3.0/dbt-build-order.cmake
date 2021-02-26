93e9c3242f8d93b19636446e71548ce59551f5da
Alessandro Thea <alessandro.thea@cern.ch>
2021-02-26 17:00:45 +0100

    Bringing in new dbt-settings format

--- configs/dunedaq-v2.3.0/dbt-build-order.cmake
+++ configs/dunedaq-v2.3.0/dbt-build-order.cmake
@@ -7,4 +7,4 @@
 # development area, the developer is encouraged to add it to its
 # appropriate place in this list
 
-set(build_order "daq-cmake" "logging" "cmdlib" "restcmd" "appfwk" "listrev" "daqdemos" "ipm" "dataformats" "dfmessages" "dfmodules" "readout" "trigemu" "minidaqapp")
+set(build_order "daq-cmake" "ers" "logging" "cmdlib" "restcmd" "opmonlib" "appfwk" "listrev" "daqdemos" "ipm" "serialization" "nwqueueadapters" "dataformats" "dfmessages" "dfmodules" "readout" "trigemu" "minidaqapp")
--- configs/dunedaq-v2.3.0/dbt-settings.sh
+++ configs/dunedaq-v2.3.0/dbt-settings.sh
@@ -1,37 +1,48 @@
 dune_products_dirs=(
-    "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-v2.2.0/externals"
-    "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-v2.2.0/packages"
-    #"/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products" 
-    #"/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products_dev" 
+    # "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-develop/externals"
+    # "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-develop/packages"
+    "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products" 
+    "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products_dev" 
 )
 
-dune_products=(
+dune_systems=(
+    "gcc v8_2_0"
+    "python v3_8_3b"
+    )
+
+dune_devtools=(
     "cmake v3_17_2"
-    "cetlib v3_11_01 e19:prof"
     "gdb v9_2"
+    )
+
+dune_externals=(
+    "cetlib v3_11_01 e19:prof"
     "TRACE v3_16_02"
     "folly v2020_05_25a e19:prof"
-    "ers v0_26_00d e19:prof"
     "nlohmann_json v3_9_0c e19:prof"
     "ninja v1_10_0"
     "pistache v2020_10_07 e19:prof"
     "highfive v2_2_2b e19:prof"
-    "cppzmq v4_3_0 e19:prof"
     "zmq v4_3_1c e19:prof"
+    "cppzmq v4_3_0 e19:prof"
+    "msgpack_c v3_3_0 e19:prof"
+    )
+
+dune_daqpackages=(
     # Note: "daq_cmake" with underscore is the UPS product name.
     # One can use either "daq-cmake" or "daq_cmake" in this file.
-    "daq-cmake v1_2_3 e19:prof"
-    "cmdlib v1_0_2b e19:prof"
-    "restcmd v1_0_3 e19:prof"
-    "appfwk v2_1_0 e19:prof"
-    "listrev v2_0_1b e19:prof"
-    "dataformats v1_0_0 e19:prof"
-    "dfmessages v1_0_0 e19:prof"
-    "dfmodules v1_1_1 e19:prof"
-    "trigemu v1_0_0 e19:prof"
-    "readout v1_0_1 e19:prof"
-    "minidaqapp v1_2_0 e19:prof"
-    "ipm v1_2_0 e19:prof"
+    "daq-cmake v1.3.1 e19:prof"
+    "ers v1.1.0 e19:prof"
+    "logging v1.0.0 e19:prof"
+    "cmdlib v1.1.0 e19:prof"
+    # "restcmd develop e19:prof"
+    # "appfwk develop e19:prof"
+    # "listrev develop e19:prof"
+    # "dataformats develop e19:prof"
+    # "dfmessages develop e19:prof"
+    # "dfmodules develop e19:prof"
+    # "trigemu develop e19:prof"
+    # "readout develop e19:prof"
+    # "minidaqapp develop e19:prof"
+    # "ipm develop e19:prof"
 )
-
-dune_python_version="v3_8_3b"
