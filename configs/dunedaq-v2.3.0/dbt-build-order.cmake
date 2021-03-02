d75a2cc62ae30c3f86209186a652b1bf71894cc1
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2021-03-02 10:18:53 -0600

    update build order for dunedaq-v2.3.0

--- configs/dunedaq-v2.3.0/dbt-build-order.cmake
+++ configs/dunedaq-v2.3.0/dbt-build-order.cmake
@@ -7,4 +7,4 @@
 # development area, the developer is encouraged to add it to its
 # appropriate place in this list
 
-set(build_order "daq-cmake" "ers" "logging" "cmdlib" "restcmd" "opmonlib" "appfwk" "listrev" "daqdemos" "ipm" "serialization" "nwqueueadapters" "dataformats" "dfmessages" "dfmodules" "readout" "trigemu" "minidaqapp")
+set(build_order "daq-cmake" "ers" "logging" "cmdlib" "rcif" "restcmd" "opmonlib" "appfwk" "listrev" "daqdemos" "ipm" "serialization" "nwqueueadapters" "dataformats" "dfmessages" "dfmodules" "readout" "trigemu" "minidaqapp")
