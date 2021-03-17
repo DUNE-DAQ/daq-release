810dfd62c3a14cedd2cf25af59e4185d78587deb
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2021-03-17 13:20:52 -0500

    check existence of tagged dbt in cvmfs

--- setup_dunedaq.sh
+++ setup_dunedaq.sh
@@ -10,5 +10,15 @@ fi
 
 function setup_dbt
 {
+	cvmfs_path="/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/tools/dbt"
+	if [ ! -d ${cvmfs_path}/daq-buildtools-${1} ]; then
+		echo -e "\033[31mError: Tagged version ${1} of daq-buildtools not found under ${cvmfs_path}\e[0m"
+		echo "Available versions are: "
+		for i in `ls ${cvmfs_path}`; do
+			echo ${i/daq-buildtools-/};
+		done
+		return
+	fi
+
 	source /cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/tools/dbt/daq-buildtools-${1}/dbt-setup-env.sh
 }
