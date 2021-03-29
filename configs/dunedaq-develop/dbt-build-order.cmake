14d91cf3e06912022c696aec0a73a35a25eb99d2
Pengfei Ding <dingpf@fnal.gov>
2021-03-29 09:44:14 +0000

    updated develop configs and added create_nightly.sh

--- configs/dunedaq-develop/dbt-build-order.cmake
+++ configs/dunedaq-develop/dbt-build-order.cmake
@@ -7,4 +7,4 @@
 # development area, the developer is encouraged to add it to its
 # appropriate place in this list
 
-set(build_order "daq-cmake" "logging" "cmdlib" "restcmd" "appfwk" "listrev" "daqdemos" "ipm" "dataformats" "dfmessages" "dfmodules" "readout" "trigemu" "minidaqapp")
+set(build_order "daq-cmake" "ers" "logging" "cmdlib" "rcif" "restcmd" "opmonlib" "appfwk" "listrev" "daqdemos" "ipm" "serialization" "nwqueueadapters" "dataformats" "dfmessages" "dfmodules" "readout" "flxlibs" "trigemu" "influxopmon" "minidaqapp" "timing" "timinglibs")
--- configs/dunedaq-develop/dbt-settings.sh
+++ configs/dunedaq-develop/dbt-settings.sh
@@ -1,36 +1,31 @@
 dune_products_dirs=(
-    "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-develop/externals"
-    "/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/releases/dunedaq-develop/packages"
-    #"/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products" 
-    #"/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products_dev" 
+    "/scratch/releases/dunedaq-develop/externals"
+    "/scratch/releases/dunedaq-develop/packages"
 )
 
-dune_products=(
-    "cmake v3_17_2"
-    "cetlib v3_11_01 e19:prof"
-    "gdb v9_2"
-    "TRACE v3_16_02"
-    "folly v2020_05_25a e19:prof"
-    "ers v0_26_00d e19:prof"
-    "nlohmann_json v3_9_0c e19:prof"
-    "ninja v1_10_0"
-    "pistache v2020_10_07 e19:prof"
-    "highfive v2_2_2b e19:prof"
-    "zmq v4_3_1b e19"
-    # Note: "daq_cmake" with underscore is the UPS product name.
-    # One can use either "daq-cmake" or "daq_cmake" in this file.
-    "daq-cmake develop e19:prof"
-    "cmdlib develop e19:prof"
-    "restcmd develop e19:prof"
-    "appfwk develop e19:prof"
-    "listrev develop e19:prof"
-    "dataformats develop e19:prof"
-    "dfmessages develop e19:prof"
-    "dfmodules develop e19:prof"
-    "trigemu develop e19:prof"
-    "readout develop e19:prof"
-    "minidaqapp develop e19:prof"
-    "ipm develop e19:prof"
-)
+dune_systems=(
+    "gcc               v8_2_0"
+    "python            v3_8_3b"
+    )
 
-dune_python_version="v3_8_3b"
+    dune_devtools=(
+    "cmake             v3_17_2"
+    "gdb               v9_2"
+    "ninja             v1_10_0"
+    )
+
+    dune_externals=(
+    "cetlib            v3_11_01     e19:prof"
+    "TRACE             v3_16_02"
+    "folly             v2020_05_25a e19:prof"
+    "nlohmann_json     v3_9_0c      e19:prof"
+    "pistache          v2020_10_07  e19:prof"
+    "highfive          v2_2_2b      e19:prof"
+    "zmq               v4_3_1c      e19:prof"
+    "cppzmq            v4_3_0       e19:prof"
+    "msgpack_c         v3_3_0       e19:prof"
+    "felix             v1_1_1       e19:prof"
+    "pybind11          v2_6_2       e19:prof"
+    "uhal              v2_8_0       e19:prof"
+    )
+
