94a53ef08fdeb32cffcf29781a18bd4f856522f5
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2021-04-01 07:21:15 -0500

    adding script to create ups products area

---
+++ scripts/create-ups-products-area.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+target_products_dir=$PWD/products
+
+while getopts ":p:t:h" opt; do
+  case ${opt} in
+    t )
+       target_products_dir=$OPTARG
+       ;;
+    h )
+      echo "Usage:"
+      echo "    create-ups-products-area.sh  -h Display this help message."
+      echo "    [-t] <new products dir>"
+      exit 0
+      ;;
+   \? )
+     echo "Invalid Option: -$OPTARG" 1>&2
+     exit 1
+     ;;
+  esac
+done
+
+if [ -d $target_products_dir ]; then
+    echo "Error: Directory $target_products_dir exists, it must be empty, exit now."
+    exit 1
+fi
+
+mkdir -p $target_products_dir
+pushd $target_products_dir
+curl https://raw.githubusercontent.com/DUNE-DAQ/daq-release/develop/misc/ups-products-area.tar.gz|tar xf
+popd
+
+echo "Info: New UPS products area has been created at: $target_products_dir"
+echo "Info: to use it, run 'source $target_products_dir/setup'"
