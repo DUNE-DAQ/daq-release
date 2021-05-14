58d3c486e4168fa4dc45a6fb8d4a990955514c10
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2021-05-14 00:28:57 -0500

    adding configs for dunedaq-v2.5.0

---
+++ configs/dunedaq-v2.5.0/dbt-settings.sh
@@ -0,0 +1,44 @@
+dune_products_dirs=(
+    #"/cvmfs/dunedaq.opensciencegrid.org/releases/dunedaq-v2.4.0/packages"
+    #"/cvmfs/dunedaq.opensciencegrid.org/releases/dunedaq-v2.4.0/externals"
+    "/cvmfs/dunedaq.opensciencegrid.org/products"
+)
+
+dune_systems=(
+    "gcc               v8_2_0"
+    "python            v3_8_3b"
+)
+
+dune_devtools=(
+    "cmake             v3_17_2"
+    "gdb               v9_2"
+    "ninja             v1_10_0"
+)
+
+dune_externals=(
+    "cetlib            v3_11_01     e19:prof"
+    "TRACE             v3_16_02"
+    "folly             v2020_05_25a e19:prof"
+    "nlohmann_json     v3_9_0c      e19:prof"
+    "pistache          v2020_10_07  e19:prof"
+    "highfive          v2_2_2b      e19:prof"
+    "zmq               v4_3_1c      e19:prof"
+    "cppzmq            v4_3_0       e19:prof"
+    "msgpack_c         v3_3_0       e19:prof"
+    "felix             v1_1_1       e19:prof"
+    "pybind11          v2_6_2       e19:prof"
+    "uhal              v2_8_0       e19:prof"
+    "cpr               v1_5_2       e19:prof"
+)
+
+dune_daqpackages=(
+    "daq_cmake         v1_3_4       e19:prof"
+    "ers               v1_1_2b      e19:prof"
+    "logging           v1_0_1c      e19:prof"
+    "cmdlib            v1_1_2b      e19:prof"
+    "restcmd           v1_1_2b      e19:prof"
+    "opmonlib          v2_0_0       e19:prof"
+    "rcif              v1_1_0       e19:prof"
+    "appfwk            v2_3_0       e19:prof"
+    "listrev           v2_1_2       e19:prof"
+)
