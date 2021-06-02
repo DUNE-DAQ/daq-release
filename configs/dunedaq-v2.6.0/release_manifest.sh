d818460e4dc1e0bdabb5855f1de2ceb17c88b77b
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2021-06-02 15:39:37 -0500

    bump versions before releasing dunedaq-v2.6.0

--- configs/dunedaq-v2.6.0/release_manifest.sh
+++ configs/dunedaq-v2.6.0/release_manifest.sh
@@ -38,8 +38,8 @@ dune_ups=(
 )
 
 dune_extras=(
-  "daq_buildtools    v3_0_1                  "
-  "daq_release       v1_5_1                  "
+  "daq_buildtools    v3_0_2                  "
+  "daq_release       v1_6_0                  "
 )
 
 dune_packages=(
