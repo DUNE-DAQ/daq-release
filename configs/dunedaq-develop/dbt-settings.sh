1022cbaf246938a6eb6453c695b86b44789933c3
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2021-06-03 09:59:36 -0500

    update develop release config

--- configs/dunedaq-develop/dbt-settings.sh
+++ configs/dunedaq-develop/dbt-settings.sh
@@ -6,15 +6,15 @@ dune_products_dirs=(
 dune_systems=(
     "gcc               v8_2_0"
     "python            v3_8_3b"
-    )
+)
 
-    dune_devtools=(
+dune_devtools=(
     "cmake             v3_17_2"
     "gdb               v9_2"
     "ninja             v1_10_0"
-    )
+)
 
-    dune_externals=(
+dune_externals=(
     "cetlib            v3_11_01     e19:prof"
     "TRACE             v3_16_02"
     "folly             v2020_05_25a e19:prof"
@@ -28,5 +28,33 @@ dune_systems=(
     "pybind11          v2_6_2       e19:prof"
     "uhal              v2_8_0       e19:prof"
     "cpr               v1_5_2       e19:prof"
-    )
+)
+
+dune_daqpackages=(
+    "daq_cmake         v1_4_2          e19:prof"
+    "ers               v1_1_2c         e19:prof"
+    "erses             v1_0_0          e19:prof"
+    "logging           v1_0_2          e19:prof"
+    "cmdlib            v1_1_2c         e19:prof"
+    "restcmd           v1_1_2c         e19:prof"
+    "opmonlib          v1_3_0          e19:prof"
+    "rcif              v1_1_0b         e19:prof"
+    "appfwk            v2_3_1          e19:prof"
+    "listrev           v2_1_3b         e19:prof"
+    "serialization     v1_2_2          e19:prof"
+    "flxlibs           v1_1_0          e19:prof"
+    "dataformats       v2_1_0          e19:prof"
+    "dfmessages        v2_1_0          e19:prof"
+    "dfmodules         v2_1_0          e19:prof"
+    "trigemu           v2_3_0          e19:prof"
+    "readout           v1_3_0          e19:prof"
+    "minidaqapp        v3_0_0          e19:prof"
+    "ipm               v2_1_2          e19:prof"
+    "timing            v5_4_1          e19:prof"
+    "timinglibs        v1_1_1          e19:prof"
+    "trigger           v1_0_4          e19:prof"
+    "triggeralgs       v0_2_2          e19:prof"
+    "influxopmon       v1_3_0          e19:prof"
+    "nwqueueadapters   v1_3_1          e19:prof"
+)
 
