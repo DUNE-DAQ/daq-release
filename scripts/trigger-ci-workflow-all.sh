694627defc6b8e74534d0fb4292718540eb2be64
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2022-03-11 13:50:06 -0600

    add a script to trigger CI jobs across all DAQ repos

---
+++ scripts/trigger-ci-workflow-all.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+dune_packages_with_ci=(
+ "daq-cmake"       
+ "ers"             
+ "erskafka"        
+ "logging"         
+ "opmonlib"        
+ "cmdlib"          
+ "rcif"            
+ "restcmd"         
+ "utilities"       
+ "ipm"             
+ "networkmanager"  
+ "appfwk"          
+ "listrev"         
+ "daqdemos"        
+ "serialization"   
+ "nwqueueadapters" 
+ "daqdataformats"  
+ "detdataformats"  
+ "detchannelmaps"  
+ "dfmessages"      
+ "trigemu"         
+ "triggeralgs"     
+ "timing"          
+ "timinglibs"      
+ "hdf5libs"        
+ "trigger"         
+ "readoutlibs"     
+ "fdreadoutlibs"   
+ "ndreadoutlibs"   
+ "readoutmodules"  
+ "flxlibs"         
+ "dfmodules"       
+ "influxopmon"     
+ "kafkaopmon"      
+ "daqconf"         
+ "dqm"             
+ "lbrulibs"        
+ "wibmod"          
+ "sspmodules"      
+ "uhallibs"
+ "dtpcontrols"
+ "rawdatautils"
+)
+
+function trigger_ci_all {
+  prd_list_name=$1[@]
+  workflow_file=$2
+  prd_list=("${!prd_list_name}")
+  for prod in "${prd_list[@]}"; do
+    iprd_arr=(${prod})
+    prod_name=${iprd_arr[0]//_/-}
+    # Trigger workflow 
+    #last_failed_scheudle_run=$(gh run -R DUNE-DAQ/trigemu list|grep build-develop| grep failure| head -n 1|egrep -o '[[:digit:]]{10}')
+    gh workflow -R DUNE-DAQ/${prod_name} run $workflow_file
+  done
+}
+
+
+trigger_ci_all dune_packages_with_ci dunedaq-develop-cpp-ci.yml
