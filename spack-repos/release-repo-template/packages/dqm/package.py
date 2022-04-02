aac41c7acac2b4c72423c58fc4873d7219c43188
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2022-04-02 06:40:10 -0500

    adding ~mpi for fftw in dqm/package.py; otherwise openmpi will be added to the concretization result

--- spack-repos/release-repo-template/packages/dqm/package.py
+++ spack-repos/release-repo-template/packages/dqm/package.py
@@ -35,7 +35,7 @@ class Dqm(CMakePackage):
     depends_on("openssl")
     depends_on("krb5")
     depends_on("cyrus-sasl")
-    depends_on("fftw")
+    depends_on("fftw ~mpi")
 
     # DBT_DEBUG is used by daq-cmake to set compiler options
     def cmake_args(self):
