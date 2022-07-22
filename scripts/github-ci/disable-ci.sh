faf2ceeb1577ec005fbbae0ecd1cdca34fad2e63
Pengfei Ding <dingpf@fnal.gov>
2022-07-22 10:58:01 -0500

    update disable-ci script

--- scripts/github-ci/disable-ci.sh
+++ scripts/github-ci/disable-ci.sh
@@ -1,78 +1,20 @@
 #!/bin/bash
 
-function disable_package_ci {
-    iprd_arr="$@"
-    iprd_arr="${iprd_arr#\"}"
-    iprd_arr="${iprd_arr%\"}"
-    iprd_arr=($iprd_arr)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source $SCRIPT_DIR/repo.sh
+
+function trigger_ci_all {
+  prd_list_name=$1[@]
+  workflow_file=$2
+  prd_list=("${!prd_list_name}")
+  for prod in "${prd_list[@]}"; do
+    iprd_arr=(${prod})
     prod_name=${iprd_arr[0]//_/-}
-    #ci_status=$(gh run list -R DUNE-DAQ/$prod_name|grep schedule |head -n 1)
-    ci_status=$(gh api --method PUT -H "Accept: application/vnd.github+json" /repos/DUNE-DAQ/${prod_name}/actions/workflows/auto_approve.yaml/disable)
-    echo "$prod_name $ci_status"
+    # Trigger workflow 
+    #last_failed_scheudle_run=$(gh run -R DUNE-DAQ/trigemu list|grep build-develop| grep failure| head -n 1|egrep -o '[[:digit:]]{10}')
+    gh workflow -R DUNE-DAQ/${prod_name} disable $workflow_file
+  done
 }
 
-update_all=0
-branch_name="not_set"
-release_manifest_file="./release_manifest.sh"
-outdir="sourcecode"
-while getopts ":f:p:b:no:ah" opt; do
-  case ${opt} in
-    f )
-       release_manifest_file=$OPTARG
-       ;;
-    p )
-       package_name=$OPTARG
-       ;;
-    b )
-       branch_name=$OPTARG
-       ;;
-    n )
-       NO_RELEASE_CHECK=true
-       ;;
-    a )
-       update_all=1
-       ;;
-    h )
-      echo "Usage:"
-      echo "    checkout-package.sh  -h Display this help message."
-      echo "    <-f> <release manifest file>"
-      echo "    [-p] <package name>"
-      echo "    [-b] <branch name>"
-      echo "    [-a] <checkout all DAQ packages>"
-      exit 0
-      ;;
-   \? )
-     echo "Invalid Option: -$OPTARG" 1>&2
-     exit 1
-     ;;
-  esac
-done
 
-
-shift $((OPTIND -1))
-
-if [ ! -f "$release_manifest_file" ]; then
-    echo "[Error]: $release_manifest_file is not found, exit now."
-    exit 2
-else
-    source $release_manifest_file
-fi
-
-release_manifest_file=`realpath $release_manifest_file`
-
-tmp_dir=$(mktemp -d -t ci_status_dunedaq_XXXXXXXXXX)
-pushd $tmp_dir
-git clone --quiet https://github.com/DUNE-DAQ/daq-release > /dev/null
-cd daq-release
-
-if [ "$update_all" -eq "1" ]; then
-    for ((i=0; i < ${#dune_packages[@]}; i++)); do
-        disable_package_ci ${dune_packages[i]}
-    done
-else
-    prd_array="$package_name $branch_name"
-    disable_package_ci $prd_array
-fi
-popd
-
-rm -rf $tmp_dir
+disable_ci_all dune_packages_with_ci auto_approve.yml
