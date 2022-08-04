9fe0ac10deea6872dd56641d6aff8d3d8cb9f519
Pengfei Ding <dingpf@fnal.gov>
2022-08-04 12:44:17 -0500

    remove obselete UPS releated scripts/files

--- scripts/ups_build_scripts/ups/v6_0_8/Linux64bit+3.10-2.17/inc/upsuname.h
+++
@@ -1,9 +0,0 @@
-
-void ups_append_OS(char *flavor);
-void ups_append_MACHINE(char *buf);
-void ups_append_release(char *flavor);
-void ups_make_default_quals_optional();
-char *ups_get_default_quals();
-char *ups_get_default_host();
-char *ups_get_default_B();
-int ups_64bit_check();
