34c7e4764579862177c058e81ed785a153890f34
Pengfei Ding <dingpf@fnal.gov>
2022-09-29 11:20:23 -0500

    syncing track_new_issues.yml; previous commit: Author: Pengfei Ding <dingpf@fnal.gov> Date:   Thu Sep 29 11:07:48 2022 -0500
        syncing track_new_prs.yml; previous commit: Author: Pengfei Ding <dingpf@fnal.gov>
        Date:   Mon Sep 26 10:47:11 2022 -0500
    
            upgrade nanorc and daqconf versions

--- scripts/github-ci/sync-ci-track-pr-workflow-to-template.sh
+++ scripts/github-ci/sync-ci-track-pr-workflow-to-template.sh
@@ -3,17 +3,25 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/repo.sh
 
+dune_packages=(
+	"elisa_client_api"
+	"grafana-dashboards"
+	"daq-systemtests"
+)
+
 function git_checkout_and_update_ci {
   prd_list_name=$1[@]
   workflow_file=$2
   prd_list=("${!prd_list_name}")
   for prod in "${prd_list[@]}"; do
     iprd_arr=(${prod})
-    prod_name=${iprd_arr[0]//_/-}
+    prod_name=${iprd_arr[0]}
+    #prod_name=${iprd_arr[0]//_/-}
     echo "--------------------------------------------------------------"
     echo "********************* $prod_name *****************************"
     git clone --quiet git@github.com:DUNE-DAQ/${prod_name}.git -b develop
     pushd ${prod_name}
+    mkdir -p ./github/workflows
     cp $workflow_file .github/workflows
     git add .github/workflows
     old_message=`git log -1|grep -v "^commit"`
@@ -31,8 +39,20 @@ pushd $tmp_dir
 git clone https://github.com/DUNE-DAQ/.github.git
 
 git_checkout_and_update_ci dune_packages $tmp_dir/.github/workflow-templates/track_new_issues.yml
+
+popd
+
+rm -rf $tmp_dir
+
+tmp_dir=$(mktemp -d -t cvmfs_dunedaq_release_XXXXXXXXXX)
+
+pushd $tmp_dir
+
+git clone https://github.com/DUNE-DAQ/.github.git
+
 git_checkout_and_update_ci dune_packages $tmp_dir/.github/workflow-templates/track_new_prs.yml
 
 popd
 
 rm -rf $tmp_dir
+
