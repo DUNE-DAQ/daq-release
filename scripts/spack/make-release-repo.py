17c6f61a0023e1f499b947e8a44f64e66acd200f
Pengfei Ding <dingpf@fnal.gov>
2022-11-09 13:41:59 -0600

    skip build_type for py-modules

--- scripts/spack/make-release-repo.py
+++ scripts/spack/make-release-repo.py
@@ -160,7 +160,10 @@ class DAQRelease:
                 else:
                     if "dunedaq" not in self.rdict["release"]:
                         iver = self.rdict["release"]
-                    lines += f'\n        depends_on(f"{iname}@{iver} build_type={{build_type}}", when=f"build_type={{build_type}}")'
+                    if iname.startswith("py-"):
+                        lines += f'\n        depends_on(f"{iname}@{iver}")'
+                    else:
+                        lines += f'\n        depends_on(f"{iname}@{iver} build_type={{build_type}}", when=f"build_type={{build_type}}")'
             lines += '\n'
             ipkg_dir = os.path.join(repo_dir, ipkg)
             os.makedirs(ipkg_dir)
