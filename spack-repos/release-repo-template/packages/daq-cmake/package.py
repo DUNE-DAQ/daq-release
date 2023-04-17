b4fb5d50866a3b8667885d9096480577cdc92230
Pengfei Ding <dingpf@fnal.gov>
2023-04-17 09:02:41 -0500

    add DAQ_CMAKE_SHARE into spack recipe

--- spack-repos/release-repo-template/packages/daq-cmake/package.py
+++ spack-repos/release-repo-template/packages/daq-cmake/package.py
@@ -27,4 +27,5 @@ class DaqCmake(CMakePackage):
             return ["-DDBT_DEBUG=false"]
 
     def setup_run_environment(self, env):
+        env.set(self.__module__.split(".")[-1].upper().replace("-", "_") + "_SHARE", self.prefix + "/share" )
         env.prepend_path('PYTHONPATH', self.prefix.lib + "64/python")
