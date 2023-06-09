47f4f1ce803b105072a051930cf309b9af851640
Pengfei Ding <dingpf@fnal.gov>
2023-06-08 21:29:30 -0500

    fix oksconfig package.py

--- spack-repos/release-repo-template/packages/oksconfig/package.py
+++ spack-repos/release-repo-template/packages/oksconfig/package.py
@@ -7,7 +7,7 @@
 from spack import *
 
 
-class Oksoksdbinterfaces(CMakePackage):
+class Oksconfig(CMakePackage):
     """Run Control related"""
 
     homepage = "https://dune-daq-sw.readthedocs.io/en/latest/packages/oksconfig/"
