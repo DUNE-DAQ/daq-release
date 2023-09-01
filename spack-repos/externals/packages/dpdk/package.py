f756d2af93fb0f2c2de642a7807f3ca53d7706b4
Pengfei Ding <9438483+dingp@users.noreply.github.com>
2023-08-31 22:14:35 -0500

    update dpdk to have libarchive deps

--- spack-repos/externals/packages/dpdk/package.py
+++ spack-repos/externals/packages/dpdk/package.py
@@ -15,7 +15,6 @@ class Dpdk(MesonPackage):
 
     maintainers = ['jcfreeman2']
 
-    version('22.03.1', url='https://fast.dpdk.org/rel/dpdk-22.03.tar.xz',   sha256='b2de5f08bcd5733f90d4d7e6c032515908dad8fc8d267ac6a253442d9b83a7c5')
     version('22.03',   sha256='b2de5f08bcd5733f90d4d7e6c032515908dad8fc8d267ac6a253442d9b83a7c5')
     version('21.11.1', sha256='e0d1c442087ead6759d129ce7d7e3b87b4a01cd71047c621ebc35bb637027658')
     version('21.11',   sha256='3246e3ed68ee2b369a5d8be2c06cf108a669e157f4d41c5bcbbb216bf5abd3a1')
@@ -27,6 +26,7 @@ class Dpdk(MesonPackage):
     depends_on("libbsd")
     depends_on("numactl")
     depends_on("openssl")
+    depends_on("libarchive")
 
     def setup_run_environment(self, env):
         env.set(self.__module__.split(".")[-1].upper().replace("-", "_") + "_INC", self.prefix + "/include" )
