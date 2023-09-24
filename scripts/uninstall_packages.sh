97820d0dcb28725c13fa7a74981e5056b8881216
John Christian Freeman <jofreema@np04-srv-019.cern.ch>
2023-09-24 18:35:17 +0200

    JCF: Issue #129: add a bit of commenting to the uninstallation script, also drop cli11

--- scripts/uninstall_packages.sh
+++ scripts/uninstall_packages.sh
@@ -3,15 +3,28 @@
 
 . /cvmfs/dunedaq.opensciencegrid.org/spack/externals/ext-v1.1/spack-0.20.0-gcc-12.1.0/spack-installation/share/spack/setup-env.sh
 
-for package in cetmodules ca-certificates-mozilla texinfo meson autoconf-archive autoconf \
+# cetmodules a build-only dependency
+# cli11 is header-only
+# cmake, ninja, gdb not needed for a run time environment
+externals_to_remove="cetmodules cli11 cmake ninja gdb"
+
+# Other packages which are removed here are at least one of the following
+# (1) build-only dependencies
+# (2) clearly used for development purposes only
+# (3) header-only (e.g. czmq)
+
+for package in ca-certificates-mozilla texinfo meson autoconf-archive autoconf \
 	       automake libtool czmq docbook-xsl docbook-xml cppzmq yaml-cpp gawk \
 	       bison diffutils re2c \
 	       bdftopcf mkfontdir gperf which flex findutils nasm xkbcomp \
-	       cmake ninja gdb \
-	       py-pip py-wheel py-hatch-fancy-pypi-readme py-flit-core py-hatch-vcs py-calver py-cython; do
+	       py-pip py-wheel py-hatch-fancy-pypi-readme py-flit-core py-hatch-vcs py-calver py-cython \
+               $externals_to_remove ; do
     spack uninstall -y --all $package
 done
 
+# Although a link dependency of cetlib, catch2 is only a
+# "multi-paradigm test framework" for C++. Not needed in a run
+# environment.
 
 for package in catch2; do
     spack uninstall -y --all --force $package
