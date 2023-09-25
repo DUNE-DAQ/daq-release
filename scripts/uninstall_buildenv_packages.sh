ac3fe99cf2ba84be00ddb2d111a464c29d3f336e
John Freeman <jcfree@fnal.gov>
2023-09-25 15:32:17 -0500

    JCF: Issue #129: more refining of the list of installed packages, including reinstatement of ninja (needed for spack load, it appears) and removal of a couple more packages

--- scripts/uninstall_buildenv_packages.sh
+++ scripts/uninstall_buildenv_packages.sh
@@ -4,20 +4,21 @@
 . /cvmfs/dunedaq.opensciencegrid.org/spack/externals/ext-v1.1/spack-0.20.0-gcc-12.1.0/spack-installation/share/spack/setup-env.sh
 
 # cetmodules a build-only dependency
-# cli11 is header-only
-# cmake, ninja, gdb not needed for a run time environment
-externals_to_remove="cetmodules cli11 cmake ninja gdb"
+# cli11 and cppzmq are header-only
+# cmake and gdb not needed for a run time environment
+# ninja appears to be needed by "spack load". Therefore not in this list...
+externals_to_remove="cetmodules cli11 cmake gdb cppzmq"
 
 # Other packages which are removed here are at least one of the following
-# (1) build-only dependencies
-# (2) clearly used for development purposes only
-# (3) header-only (e.g. czmq)
+# (1) build-only dependencies (e.g. czmq)
+# (2) clearly used for development purposes only (e.g., autoconf)
+# (3) header-only (e.g. cppzmq)
 
 for package in ca-certificates-mozilla texinfo meson autoconf-archive autoconf \
-	       automake libtool czmq docbook-xsl docbook-xml cppzmq yaml-cpp gawk \
+	       automake gmake libtool docbook-xml docbook-xsl czmq yaml-cpp gawk \
 	       bison diffutils re2c \
-	       bdftopcf mkfontdir gperf which flex findutils nasm xkbcomp \
-	       py-pip py-wheel py-hatch-fancy-pypi-readme py-flit-core py-hatch-vcs py-calver py-cython \
+	       bdftopcf mkfontdir gperf which flex findutils nasm xkbcomp \ # dbe build-only dependencies on this line
+	       py-pip py-wheel py-hatch-fancy-pypi-readme py-flit-core py-hatch-vcs py-calver py-cython \ # Python packages on this line
                $externals_to_remove ; do
     spack uninstall -y --all $package
 done
