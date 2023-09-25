542654e4bc73d58207d0f5e3ee6e02df84afcc68
John Christian Freeman <jofreema@np04-srv-015.cern.ch>
2023-09-25 22:48:11 +0200

    JCF: Issue #129: fix syntax error introduced by commenting in uninstall_buildenv_packages.sh

--- scripts/uninstall_buildenv_packages.sh
+++ scripts/uninstall_buildenv_packages.sh
@@ -3,11 +3,17 @@
 
 . /cvmfs/dunedaq.opensciencegrid.org/spack/externals/ext-v1.1/spack-0.20.0-gcc-12.1.0/spack-installation/share/spack/setup-env.sh
 
+# For externals:
 # cetmodules a build-only dependency
 # cli11 and cppzmq are header-only
 # cmake and gdb not needed for a run time environment
 # ninja appears to be needed by "spack load". Therefore not in this list...
 externals_to_remove="cetmodules cli11 cmake gdb cppzmq"
+
+python_packages_to_remove="py-pip py-wheel py-hatch-fancy-pypi-readme py-flit-core py-hatch-vcs py-calver py-cython"
+
+# Packages dbe depends on which aren't part of fddaq
+dbe_packages_to_remove="bdftopcf mkfontdir gperf which flex findutils nasm xkbcomp"
 
 # Other packages which are removed here are at least one of the following
 # (1) build-only dependencies (e.g. czmq)
@@ -15,10 +21,10 @@ externals_to_remove="cetmodules cli11 cmake gdb cppzmq"
 # (3) header-only (e.g. cppzmq)
 
 for package in ca-certificates-mozilla texinfo meson autoconf-archive autoconf \
-	       automake gmake libtool docbook-xml docbook-xsl czmq yaml-cpp gawk \
+	       automake gmake libtool docbook-xsl docbook-xml czmq yaml-cpp gawk \
 	       bison diffutils re2c \
-	       bdftopcf mkfontdir gperf which flex findutils nasm xkbcomp \ # dbe build-only dependencies on this line
-	       py-pip py-wheel py-hatch-fancy-pypi-readme py-flit-core py-hatch-vcs py-calver py-cython \ # Python packages on this line
+	       $dbe_packages_to_remove \
+	       $python_packages_to_remove \
                $externals_to_remove ; do
     spack uninstall -y --all $package
 done
