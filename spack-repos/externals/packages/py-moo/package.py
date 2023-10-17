ad66d77b79ba0033b01182ce462a4354b012ff3d
John Freeman <jcfree@fnal.gov>
2023-10-17 15:42:44 -0500

    JCF: add support for moo v0.6.7

--- spack-repos/externals/packages/py-moo/package.py
+++ spack-repos/externals/packages/py-moo/package.py
@@ -10,13 +10,14 @@ class PyMoo(PythonPackage):
     """Model oriented objects, tools for code generation."""
 
     homepage = "https://brettviren.github.io/moo"
-    url      = "https://github.com/brettviren/moo/archive/0.6.3.tar.gz"
+    url      = "https://github.com/brettviren/moo/archive/0.6.7.tar.gz"
     git      = "https://github.com/brettviren/moo.git"
 
     maintainers = ['jcfreeman2']
 
     version('0.5.7', sha256='f881e396d521dbd1937d4e25949e80770dc8bad03e29888f63aa613bcce82222')
     version('0.6.3', sha256='f8be51ed4e896bb66329d3d66185b4da375d0964f6a0e582d32565032eaad249')
+    version('0.6.7', sha256='f155c72e2d0f384fce384fa1c5b2fbab17e09aeb1bd4b10454a9b11d95eef8e2')
 
 
     depends_on('py-click', type=('build', 'run'))
