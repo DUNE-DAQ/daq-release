de67aebe8764c44eb823e3bad069671a3ca21b94
John Freeman <jcfree@fnal.gov>
2023-12-01 10:37:12 -0600

    JCF: very early check for feasilibity of a common file with constants, functions, etc. for workflows

---
+++ .github/workflows/wf-setup-tools.sh
@@ -0,0 +1,5 @@
+
+export SPACK_VERSION=0.20.0
+export GCC_VERSION=12.1.0
+export EXT_VERSION=v2.0
+export SPACK_EXTERNALS=/cvmfs/dunedaq.opensciencegrid.org/spack/externals/ext-${EXT_VERSION}/spack-$SPACK_VERSION-gcc-$GCC_VERSION
