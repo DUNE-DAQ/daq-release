1555f07858050825a6c6f3d7e50d6ee4e16763bb
John Freeman <jcfree@fnal.gov>
2023-12-01 12:51:55 -0600

    JCF: put the DAQ-ification of the Spack environment into its own function

--- .github/workflows/wf-setup-tools.sh
+++ .github/workflows/wf-setup-tools.sh
@@ -25,3 +25,52 @@ function bad_function() {
   echo "About to drop a false-bomb" >&2
   false
 }
+
+function daqify_spack_environment() {
+
+   if [[ -z $SPACK_ROOT ]]; then
+       echo "Spack doesn't appear to have been set up; returning..." >&2
+       return 1
+   fi
+
+   if [[ -z $SPACK_VERSION || -z $SPACK_RELEASE || -z $SPACK_EXTERNALS ]]; then
+       echo "At least one of the environment variables you need for daqify_spack_environment isn't set; returning..." >&2
+       return 2
+   fi
+
+   if [[ ! -e spack-${SPACK_VERSION}/share/spack/setup-env.sh ]]; then
+       echo "Can't find spack-${SPACK_VERSION}/share/spack/setup-env.sh; you may be calling daqify_spack_environment from the wrong directory ($PWD). Contents are as follows:"
+       ls -ltr
+       return 3
+   fi
+
+   source spack-${SPACK_VERSION}/share/spack/setup-env.sh
+
+   echo "*********** spack arch ************ "
+   spack arch
+
+   cat <<EOF > $SPACK_ROOT/etc/spack/defaults/repos.yaml 
+          repos:
+            - ${SPACK_RELEASE}/spack-${SPACK_VERSION}/spack-repo
+            - ${SPACK_EXTERNALS}/spack-${SPACK_VERSION}/spack-repo-externals
+            - \$spack/var/spack/repos/builtin
+EOF
+
+   spack repo list
+
+   cat <<EOF  >> $SPACK_ROOT/etc/spack/defaults/upstreams.yaml  
+          upstreams:
+            spack-externals:
+              install_tree: ${SPACK_EXTERNALS}/spack-${SPACK_VERSION}/opt/spack
+EOF
+
+    cp $SPACK_EXTERNALS/spack-${SPACK_VERSION}/etc/spack/defaults/linux/compilers.yaml \
+      $SPACK_RELEASE/spack-${SPACK_VERSION}/etc/spack/defaults/linux/
+  
+    spack compiler list
+
+    \cp $SPACK_EXTERNALS/spack-${SPACK_VERSION}/etc/spack/defaults/config.yaml  $SPACK_RELEASE/spack-${SPACK_VERSION}/etc/spack/defaults/config.yaml
+    \cp $SPACK_EXTERNALS/spack-${SPACK_VERSION}/etc/spack/defaults/modules.yaml  $SPACK_RELEASE/spack-${SPACK_VERSION}/etc/spack/defaults/modules.yaml
+
+    sed -i 's/host_compatible: true/host_compatible: false/g' $SPACK_ROOT/etc/spack/defaults/concretizer.yaml
+}
