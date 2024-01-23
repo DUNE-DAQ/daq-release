deabb0fd639779a971ecadf8c780c075d61dfaac
John Freeman <jcfree@fnal.gov>
2024-01-22 18:39:13 -0600

    JCF: Issue #337: update build-release.sh so it uses soft links to be backwards compatible with daq-buildtools v7.5.0

--- .github/workflows/wf-setup-tools.sh
+++ .github/workflows/wf-setup-tools.sh
@@ -33,8 +33,7 @@ else
 fi
 
 export SPACK_VERSION=0.20.0
-export GCC_VERSION=12.1.0
-export SPACK_EXTERNALS=/cvmfs/dunedaq.opensciencegrid.org/spack/externals/ext-${EXT_VERSION}/spack-$SPACK_VERSION-gcc-$GCC_VERSION
+export SPACK_EXTERNALS=/cvmfs/dunedaq.opensciencegrid.org/spack/externals/ext-${EXT_VERSION}
 
 export DET_SPACK_AREA=$DET_RELEASE_DIR
 export BASE_SPACK_AREA=$BASE_RELEASE_DIR
