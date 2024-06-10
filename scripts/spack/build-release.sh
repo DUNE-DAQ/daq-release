a7ec0c1d155bfa7746df8d12d37c1e7d15b4182c
John Freeman <jcfree@daq.fnal.gov>
2024-06-10 15:55:48 -0500

    JCF: create an umbrella package, coredaqumbrella, meant specifically to ensure that the same OKS packages are used for coredaq and for dbe

--- scripts/spack/build-release.sh
+++ scripts/spack/build-release.sh
@@ -97,7 +97,11 @@ fi
 
 build_dbe=false
 if [[ $DET == "core" ]]; then
-    spack spec -l --reuse dbe%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 > $SPACK_AREA/spec_dbe_log.txt 2>&1
+
+    mkdir -p $SPACK_AREA/spack-installation/spack-repo/packages/coredaqumbrella
+    cp $DAQ_RELEASE_REPO/$spack_template_dir/packages/coredaqumbrella/package.py $SPACK_AREA/spack-installation/spack-repo/packages/coredaqumbrella
+
+    spack spec -l --reuse coredaqumbrella ^dbe%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 > $SPACK_AREA/spec_dbe_log.txt 2>&1
     retval=$?    
 
     cat $SPACK_AREA/spec_dbe_log.txt
@@ -107,13 +111,15 @@ if [[ $DET == "core" ]]; then
     else
 	build_dbe=false
         echo "Building dbe does not appear to be possible. As this is not (necessarily) an error, will continue..."
+	rm -rf $SPACK_AREA/spack-installation/spack-repo/packages/coredaqumbrella
     fi
 fi
 
 spack install --reuse ${DET}daq@${RELEASE_TAG}%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 || exit 7
 
 if $build_dbe; then
-    spack install --reuse dbe%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 || exit 8
+    spack install --reuse coredaqumbrella ^dbe%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 || exit 8
+    spack uninstall coredaqumbrella  # coredaqumbrella only used to avoid package reinstallation
 fi
 
 if [[ "$DET" == "fd" || "$DET" == "nd" ]]; then
