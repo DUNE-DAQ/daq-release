ae36c37e5b08dd15ad109e3ee6f17c18b13130ac
Andrew Mogan <amogan@fnal.gov>
2024-10-16 13:25:53 -0500

    Revamp CI sync script to use workflow file name instead of devline

--- scripts/github-ci/sync-ci-workflow-to-template.sh
+++ scripts/github-ci/sync-ci-workflow-to-template.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 if (( $# != 1 )); then
-    echo "Usage: $( basename $0 ) <production_v4 or develop)>" >&2
+    echo "Usage: $( basename $0 ) <workflow_file_name>" >&2
     exit 1
 fi
 
-export DEVLINE=$1
+workflow_file=$1
+common_branch="develop"
+
+if [[ "$workflow_file" == *v4*]]; then
+    echo "Using branch production_v4 for v4 workflow $workflow_file"
+    common_branch="production_v4"
+fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/repo.sh
 
 function git_checkout_and_update_ci {
-  prd_list_name=$1[@]
+  repo_list_name=$1[@]
   src_workflow_file=$2
   dest_workflow_file=$3
-  prd_list=("${!prd_list_name}")
-  for prod in "${prd_list[@]}"; do
-    iprd_arr=(${prod})
-    prod_name=${iprd_arr[0]//_/-}
+  common_branch=$4
+  repo_list=("${!repo_list_name}")
+  for repo in "${repo_list[@]}"; do
+    irepo_arr=(${prod})
+    repo_name=${irepo_arr[0]//_/-}
     echo "--------------------------------------------------------------"
-    echo "********************* $prod_name *****************************"
-
-    if [[ "$DEVLINE" == "develop" ]]; then
-	common_branch="develop"
-    elif [[ "$DEVLINE" == "production_v4" ]]; then
-	common_branch="production/v4"
-    fi
-
-    git clone --quiet https://github.com/DUNE-DAQ/${prod_name}.git -b $common_branch || exit 1
-    pushd ${prod_name}
+    echo "********************* $repo_name *****************************"
+    git clone --quiet https://github.com/DUNE-DAQ/${repo_name}.git -b $common_branch || exit 2
+    pushd ${repo_name}
     mkdir -p .github/workflows
     cp $src_workflow_file .github/workflows/$dest_workflow_file
     git add .github/workflows
     git commit -am "Syncing .github/workflows/$(basename $dest_workflow_file)"
-    git push --quiet || exit 2
+    git push --quiet || exit 3
     popd
   done
 }
@@ -42,15 +42,16 @@ tmp_dir=$(mktemp -d -t cvmfs_dunedaq_release_XXXXXXXXXX)
 
 pushd $tmp_dir
 
-git clone https://github.com/DUNE-DAQ/.github.git || exit 3
+git clone https://github.com/DUNE-DAQ/.github.git || exit 4
 
-if [[ "$DEVLINE" == "develop" ]]; then
-    workflow_file=dunedaq-develop-cpp-ci.yml
-else
-    workflow_file=dunedaq-v4-cpp-ci.yml
+existing_workflow_templates=$(ls .github/workflow-templates/*.yml)
+if [[ ! ${existing_workflow_templates[@]} =~ ${workflow_file} ]]; then 
+    echo "Invalid workflow file name provided. The available options are:" >&2
+    ls .github/workflow-templates/*.yml >&2
+    exit 5
 fi
 
-git_checkout_and_update_ci dune_packages_with_ci $tmp_dir/.github/workflow-templates/$workflow_file $workflow_file
+git_checkout_and_update_ci dune_packages_with_ci $tmp_dir/.github/workflow-templates/$workflow_file $workflow_file $common_branch
 
 popd
 
