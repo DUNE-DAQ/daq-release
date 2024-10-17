d0cdbcee26595b58def4de822d07eb2a56940439
Andrew Mogan <amogan@fnal.gov>
2024-10-17 09:34:26 -0500

    Make workflow file an argument and rename prod to repo

--- scripts/github-ci/disable-ci.sh
+++ scripts/github-ci/disable-ci.sh
@@ -4,17 +4,15 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/repo.sh
 
 function disable_ci_all {
-  prd_list_name=$1[@]
+  repo_list_name=$1[@]
   workflow_file=$2
-  prd_list=("${!prd_list_name}")
-  for prod in "${prd_list[@]}"; do
-    iprd_arr=(${prod})
-    prod_name=${iprd_arr[0]//_/-}
-    # Trigger workflow 
-    #last_failed_scheudle_run=$(gh run -R DUNE-DAQ/trigemu list|grep build-develop| grep failure| head -n 1|egrep -o '[[:digit:]]{10}')
-    gh workflow -R DUNE-DAQ/${prod_name} disable $workflow_file
+  repo_list=("${!repo_list_name}")
+  for repo in "${repo_list[@]}"; do
+    irepo_arr=(${repo})
+    repo_name=${irepo_arr[0]//_/-}
+    gh workflow -R DUNE-DAQ/${repo_name} disable $workflow_file
   done
 }
 
 
-disable_ci_all dune_packages_with_ci auto_approve.yml
+disable_ci_all dune_packages_with_ci $workflow_file
