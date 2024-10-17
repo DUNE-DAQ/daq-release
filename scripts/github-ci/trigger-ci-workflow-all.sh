d74354b72de3e32e0134feb5d4ecdd2cdafb0cc7
Andrew Mogan <amogan@fnal.gov>
2024-10-17 09:35:21 -0500

    Make trigger CI script consistent with disable script

--- scripts/github-ci/trigger-ci-workflow-all.sh
+++ scripts/github-ci/trigger-ci-workflow-all.sh
@@ -4,17 +4,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/repo.sh
 
 function trigger_ci_all {
-  prd_list_name=$1[@]
+  repo_list_name=$1[@]
   workflow_file=$2
-  prd_list=("${!prd_list_name}")
+  repo_list=("${!repo_list_name}")
   for prod in "${prd_list[@]}"; do
-    iprd_arr=(${prod})
-    prod_name=${iprd_arr[0]//_/-}
+    irepo_arr=(${repo})
+    repo_name=${irepo_arr[0]//_/-}
     # Trigger workflow 
     #last_failed_scheudle_run=$(gh run -R DUNE-DAQ/trigemu list|grep build-develop| grep failure| head -n 1|egrep -o '[[:digit:]]{10}')
-    gh workflow -R DUNE-DAQ/${prod_name} run $workflow_file
+    gh workflow -R DUNE-DAQ/${repo_name} run $workflow_file
   done
 }
 
 
-trigger_ci_all dune_packages_with_ci dunedaq-develop-cpp-ci.yml
+trigger_ci_all dune_packages_with_ci $workflow_file
