26384b16db5a15ba0018df9276e064426456ab0d
Andrew Mogan <amogan@fnal.gov>
2024-11-13 13:42:40 -0600

    Generalize branch-finding script

--- scripts/github-ci/check_if_branch_exists.sh
+++ scripts/github-ci/check_if_branch_exists.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if (( $# != 2 )); then
-    echo "Usage: $( basename $0 ) <production_v4 or develop> <release_tag>" >&2
-    echo "Example release tag format: fddaq-v4.4.x" >&2
+    echo "Usage: $( basename $0 ) <production_v4 or develop> <branch_name>" >&2
+    echo "Example usage: ./check_if_release_prep_branch_exists.sh develop prep-release/fddaq-v5.2.0" >&2
     exit 1
 fi
 
 export DEVLINE=$1
-release_tag=$2
+branch_name=$2
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/repo.sh
@@ -16,7 +16,7 @@ function check_patch {
   tmp_dir=$(mktemp -d -t cvmfs_dunedaq_release_XXXXXXXXXX)
   cd $tmp_dir
   repo_list_name=$1[@]
-  release_tag=$2
+  branch_name=$2
   repo_list=("${!repo_list_name}")
   for repo in "${repo_list[@]}"; do
     irepo_arr=(${repo})
@@ -25,11 +25,11 @@ function check_patch {
     echo "********************* $repo_name *****************************"
     git clone --quiet https://github.com/DUNE-DAQ/${repo_name}.git 
     cd ${repo_name}
-    git show-ref | awk '{print $2}' | grep "patch/$release_tag"
+    git show-ref | awk '{print $2}' | grep "${branch_name}"
     cd ..
   done
   cd ..
   rm -rf $tmp_dir
 }
 
-check_patch dune_packages_with_ci $release_tag
+check_patch dune_packages_with_ci $branch_name
