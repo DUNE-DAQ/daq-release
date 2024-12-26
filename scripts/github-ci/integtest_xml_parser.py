1bf4d61e8f949c875e0fde7370210a36ea051ca0
Andrew Mogan <amogan@fnal.gov>
2024-12-26 10:23:56 -0600

    Add xml parser script for nightly integration tests

---
+++ scripts/github-ci/integtest_xml_parser.py
@@ -0,0 +1,110 @@
+import os
+import argparse
+from pathlib import Path
+import xml.etree.ElementTree as ET
+
+def get_xml_files(directory, pattern):
+    path = Path(directory)
+    return path.rglob(pattern)
+
+def get_test_name(file_path):
+    file_name = os.path.basename(file_path)
+    # Results file names should look like minimal_system_quick_test_results.xml
+    return file_name.replace('.xml', '')
+
+def parse_junit_xml(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+
+    test_suite_name = get_test_name(file_path)
+    
+    results = []
+    for testcase in root.findall(".//testcase"):
+        test_name = testcase.get("name").split("[")[0]
+        result = "passed"
+        
+        if testcase.find("failure") is not None:
+            result = "failed"
+        elif testcase.find("error") is not None:
+            result = "error"
+        elif testcase.find("skipped") is not None:
+            result = "skipped"
+
+        results.append({
+            "test_suite_name": test_suite_name,
+            "test_name": test_name,
+            "result": result
+        })
+
+    return results
+
+def which_emoji(test_status):
+    emoji_map = {
+        'passed': ':white_check_mark:',
+        'failed': ':x:',
+        'skipped': ':grey_question:'
+    }
+    return emoji_map.get(test_status, ':shrug:')
+
+def format_markdown_row(test):
+    emoji = which_emoji(test['result'])
+    return f"| {test['test_name']} | {emoji} {test['result']} |\n"
+
+def generate_markdown_table(results, output_filename):
+    print('----------------------------')
+    with open(output_filename, 'w') as f:
+        for idx, result in enumerate(results):
+            f.write(f"# {result[idx]['test_suite_name']}\n")
+            f.write("| Test Case | Status |\n")
+            f.write("|-----------|--------|\n")
+            f.writelines(format_markdown_row(test) for test in result)
+
+    return
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse a JUnit XML file and extract test case results.")
+    parser.add_argument("--input-directory", "-d",
+                        help="Path to the integtest directory containing junit xml files.")
+    parser.add_argument("--input-file", "-i",
+                        help="Path to the JUnit XML file")
+    parser.add_argument("--output-markdown-file", "-o", 
+                        default="pytest_summary_table.md",
+                        help="Name of the output .md file for GitHub summar table.")
+    
+    args = parser.parse_args()
+
+    if args.input_directory and args.input_file:
+        print(f"Error: You must specify either an input directory or a specific file, not both.")
+        exit(1)
+
+    test_results = []
+
+    if args.input_directory:
+        if not os.path.isdir(args.input_directory):
+            print(f"Error: {args.input_directory} is not a valid directory.")
+            exit(2)
+
+        xml_files = get_xml_files(args.input_directory, "*.xml")
+        if not xml_files:
+            print(f"Error: No xml files found in {args.input_directory}.")
+            exit(3)
+
+        for file in xml_files:
+            test_results.append(parse_junit_xml(file))
+
+    elif args.input_file:
+        if not os.path.isfile(args.input_file):
+            print(f"Error: Input file {args.input_file} is invalid.")
+            exit(4)
+    
+        test_results.append(parse_junit_xml(args.input_file))
+    else:
+        print(f"Error: No input file or directory specified. Exiting...")
+        exit(5)
+
+    generate_markdown_table(test_results, args.output_markdown_file)
+
+    print(f"Markdown summary generated at: {args.output_markdown_file}")
+
+if __name__ == "__main__":
+    main()
