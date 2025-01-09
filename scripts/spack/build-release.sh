9025f33cd3db148f1726e3c3e21d9e0fe63e52d3
Andrew Mogan <amogan@fnal.gov>
2025-01-09 14:38:23 -0600

    Issue #422: Propagate exit 111 to github actions step

--- scripts/spack/build-release.sh
+++ scripts/spack/build-release.sh
@@ -123,7 +123,11 @@ echo "spack install exit code: $spack_install_exit_code"
 if [[ $spack_install_exit_code -ne 0 ]]; then
     echo "In if statement"
     if grep -qi "==> Error: FetchError: All fetchers failed" dunedaq_build_spack_install.log; then
+        echo "Exiting 111"
         exit 111 # Specific exit code for use in retry action
+    else 
+        echo "Exit 111 not found. What's in the log?"
+        cat dunedaq_build_spack_install.log
     fi
     echo "Past if statement (should not happen)"
     exit $spack_install_exit_code
