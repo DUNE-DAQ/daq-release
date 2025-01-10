b8a145146d3e8242a9bd2fd04a90f188ca058d03
Andrew Mogan <amogan@fnal.gov>
2025-01-10 13:20:55 -0600

    Revert to forced exit 111 for testing

--- scripts/spack/build-release.sh
+++ scripts/spack/build-release.sh
@@ -112,16 +112,16 @@ if [[ $DET == "core" ]]; then
     fi
 fi
 
-spack install --reuse ${DET}daq@${RELEASE_TAG}%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 || tee dunedaq_build_spack_install.log
+#spack install --reuse ${DET}daq@${RELEASE_TAG}%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 || tee dunedaq_build_spack_install.log
+bash -c "echo '==> Error: FetchError: All fetchers failed'; exit 42" 2>&1 | tee dunedaq_build_spack_install.log
 spack_install_exit_code=${PIPESTATUS[0]}
 
 if [[ $spack_install_exit_code -ne 0 ]]; then
     if grep -qi "==> Error: FetchError: All fetchers failed" dunedaq_build_spack_install.log; then
-        exit 111 # Specific exit code for use in retry action
+        exit 111 # Specific exit code to signal that we should retry
     fi
     exit $spack_install_exit_code
 fi
-echo "Past ALL the new stuff (really shouldn't happen)"
 
 #if $build_dbe; then
 #    spack install --reuse dbe%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 || exit 8
