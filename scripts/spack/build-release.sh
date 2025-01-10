690411db8f752748d07e4b8eeced209ca145900c
Andrew Mogan <amogan@fnal.gov>
2025-01-10 14:58:20 -0600

    Issue #422: Remove spack-repo directory on exit 111

--- scripts/spack/build-release.sh
+++ scripts/spack/build-release.sh
@@ -119,6 +119,7 @@ spack_install_exit_code=${PIPESTATUS[0]}
 if [[ $spack_install_exit_code -ne 0 ]]; then
     if grep -qi "==> Error: FetchError: All fetchers failed" dunedaq_build_spack_install.log; then
         rm -rf ${SPACK_AREA}/spack-installation
+        rm -rf $SPACK_EXTERNALS/spack-${SPACK_VERSION}/spack-repo
         exit 111 # Specific exit code to signal that we should retry
     fi
     exit $spack_install_exit_code
