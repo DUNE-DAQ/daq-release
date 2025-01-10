1d1bb2968b96416b84f6e1710700c94a72bd5396
Andrew Mogan <amogan@fnal.gov>
2025-01-10 13:28:54 -0600

    Issue #422: echo spack install exit code

--- scripts/spack/build-release.sh
+++ scripts/spack/build-release.sh
@@ -115,6 +115,7 @@ fi
 #spack install --reuse ${DET}daq@${RELEASE_TAG}%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 || tee dunedaq_build_spack_install.log
 bash -c "echo '==> Error: FetchError: All fetchers failed'; exit 42" 2>&1 | tee dunedaq_build_spack_install.log
 spack_install_exit_code=${PIPESTATUS[0]}
+echo "Spack exit code: $spack_install_exit_code"
 
 if [[ $spack_install_exit_code -ne 0 ]]; then
     if grep -qi "==> Error: FetchError: All fetchers failed" dunedaq_build_spack_install.log; then
