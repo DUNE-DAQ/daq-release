ead8a3cd409f85a4f2e35510b03f550698f28a5c
Andrew Mogan <amogan@fnal.gov>
2025-01-13 15:25:17 -0600

    Issue #422: Fail first, then spack install on retry

--- scripts/spack/build-release.sh
+++ scripts/spack/build-release.sh
@@ -132,21 +132,20 @@ if [[ $spack_install_exit_code -ne 0 ]]; then
             rm -rf ${SPACK_AREA}/spack-${SPACK_VERSION}
             rm -rf spack-${SPACK_VERSION}/spack-repo
             rm -rf spack-${SPACK_VERSION}/default
-            #spack install --reuse ${DET}daq@${RELEASE_TAG}%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 | tee dunedaq_build_spack_install.log
-            bash -c "echo '==> Error: FetchError: All fetchers failed'; exit 111" | tee dunedaq_build_spack_install.log
+            spack install --reuse ${DET}daq@${RELEASE_TAG}%gcc@12.1.0 build_type=RelWithDebInfo arch=linux-${OS}-x86_64 | tee dunedaq_build_spack_install.log || true
+            #bash -c "echo '==> Error: FetchError: All fetchers failed'; exit 111" | tee dunedaq_build_spack_install.log
             #bash -c "echo 'No more error!'; exit 0" 2>&1 | tee dunedaq_build_spack_install.log
             spack_install_exit_code=${PIPESTATUS[0]}
             if [[ $spack_install_exit_code -eq 0 ]]; then
                 echo "Build succeeded after attempt number $attempt"
                 break
             elif grep -qi "==> Error: FetchError: All fetchers failed" dunedaq_build_spack_install.log; then
-                echo "Retry attempt $attempt failed due to a connection FetchError."
+                echo "Retry attempt $attempt/$max_attempts failed due to a FetchError."
                 is_fetch_error=true
             else
                 echo "Build failed with a non-retryable exit code. Exiting..."
                 exit $spack_install_exit_code
             fi
-            echo "Retry attempt $attempt/$max_attempts failed due to a FetchError."
             attempt=$((attempt+1))
         done
         if [[ $attempt -gt $max_attempts && $is_fetch_error == true ]]; then
