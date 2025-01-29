8737f1c4b00399097d9e4fa5301c87083f275fc9
Andrew Mogan <amogan@fnal.gov>
2025-01-29 14:24:46 -0600

    Factor message sections into individual functions

--- scripts/github-ci/slack_payload_generator.py
+++ scripts/github-ci/slack_payload_generator.py
@@ -13,85 +13,66 @@ def get_failed_jobs(api_output_path):
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
 
+def get_header(status):
+    """Generate the header block based on the status."""
+    status_emojis = {
+        "success": ":white_check_mark:",
+        "failure": ":rotating_light:"
+    }
+    return {
+        "type": "header",
+        "text": {
+            "type": "plain_text",
+            "text": f"{status_emojis.get(status, ':grey_question:')} {status.capitalize()}: GitHub Workflow {status_emojis.get(status, ':grey_question:')}",
+            "emoji": True
+        }
+    }
+
+def get_report_section():
+    """Generate the section block with the full report link."""
+    return {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "Full report: <https://github.com/${{ github.payload.repository.full_name }}/actions/runs/${{ github.runId }}|link>."
+        }
+    }
+
+def get_failed_jobs_section(failed_jobs):
+    if not failed_jobs:
+        return None
+    failed_jobs_text = "*Failed jobs and steps:*\n"
+    for job in failed_jobs:
+        failed_jobs_text += f"- *{job['job']}*\n"
+        for step in job['steps']:
+            failed_jobs_text += f"\t:x: *{step['name']}*\n"
+
+    return {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": failed_jobs_text
+        }
+    }
+
 def generate_payload(failed_jobs, xml_files=[]):
     """
     Top-level payload generator function that determines which payload to generate based on 
     the presence of job failures. The failure information is parsed to generate a message 
     payload in Slack's BlockKit format.  
     """
-    slack_payload = None
-    if not failed_jobs:
-        slack_payload = create_success_payload()
-    else:
-        slack_payload = create_failure_payload(failed_jobs)
+    slack_payload = {"blocks": []}
+
+    workflow_status = ('failure' if failed_jobs else 'success')
+    slack_payload["blocks"].append(get_header(workflow_status))
+
+    slack_payload["blocks"].append(get_report_section())
+
+    failed_jobs_section = get_failed_jobs_section(failed_jobs)
+    if failed_jobs_section:
+        slack_payload["blocks"].append(failed_jobs_section)
+
     return slack_payload
-
-def create_success_payload():
-    return {
-        "blocks": [
-            {
-            "type": "header",
-            "text": {
-                "type": "plain_text",
-                "text": ":white_check_mark: Success: ${{ github.workflow }} :white_check_mark:",
-                "emoji": True
-            }
-            },
-            {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "Full report: <https://github.com/${{ github.payload.repository.full_name }}/actions/runs/${{ github.runId }}|link>."
-            }
-            }
-        ]
-    }
-
-def create_failure_payload(failed_jobs):
-    slack_failure_payload = {
-        "blocks": [
-            {
-            "type": "header",
-            "text": {
-                "type": "plain_text",
-                "text": ":rotating_light: Failure: ${{ github.workflow }} :rotating_light:",
-                "emoji": True
-            }
-            },
-            {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "Full report: <https://github.com/${{ github.payload.repository.full_name }}/actions/runs/${{ github.runId }}|link>."
-            }
-            }
-        ]
-    }
-    # Goal:
-    #*Failed jobs and steps:*
-    #parse_results
-    #    X Generate summary tables
-    #send_slack_message / generate_and_send_slack_message
-    #    X Parse GitHub API output
-    failed_jobs_text = "*Failed jobs and steps:*\n"
-    for job in failed_jobs:
-        failed_jobs_text += f"*{job['job']}*\n"
-        for step in job['steps']:
-            failed_jobs_text += f"\t :x: *{step['name']}*\n"
-
-    slack_failure_payload["blocks"].append({
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": failed_jobs_text
-        }
-    })
-
-    return slack_failure_payload
-
-def create_integration_test_failure_payload():
-    from .integtest_xml_parser import parse_junit_xml
-    pass
 
 def write_payload_to_file(payload, file_name='slack_payload.json'):
     try:
