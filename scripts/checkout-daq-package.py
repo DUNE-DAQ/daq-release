57a97359b1974951933ee46a715c4f8670af2345
Andrew Mogan <amogan@fnal.gov>
2025-07-11 13:07:19 -0500

    Centralize checkout token logic
    Also, improve error messages, readability, etc.

--- scripts/checkout-daq-package.py
+++ scripts/checkout-daq-package.py
@@ -26,7 +26,7 @@ def check_output(cmd, is_success_required = True):
     else:
         print("Checkout successful")
 
-def checkout_commit(repo, commit, outdir, is_success_required = True):
+def checkout_commit(repo, commit, outdir, is_success_required=True):
     cmd = textwrap.dedent(f"""
         mkdir -p {outdir} && cd {outdir} &&
         git clone https://github.com/DUNE-DAQ/{repo}.git; 
@@ -37,29 +37,29 @@ def checkout_commit(repo, commit, outdir, is_success_required = True):
     check_output(cmd, is_success_required)
     return
 
-def checkout_tag(repo, commit, outdir, is_pymodule=False):
+def checkout_tag(repo, version, outdir, is_pymodule=False):
     cmd = textwrap.dedent(f"""
         mkdir -p {outdir} && cd {outdir} &&
         git clone https://github.com/DUNE-DAQ/{repo}.git &&
         cd {repo} &&
         git fetch --tags &&
-        if ! git show-ref --tags --verify --quiet "refs/tags/{commit}"; then
-            echo "{commit} does not exist for package {repo}. Exiting..." && exit 1
+        if ! git show-ref --tags --verify --quiet "refs/tags/{version}"; then
+            echo "The tag {version} does not exist for package {repo}. Exiting..." && exit 1
         fi
-        git checkout {commit} &&
+        git checkout {version} &&
         if [[ "{is_pymodule}" == "False" ]]; then
             cmake_version=`grep "^project" CMakeLists.txt |grep ")$"|grep -oP "(([[:digit:]]+\.)([[:digit:]]+\.)([[:digit:]]+))"` &&
             tag=v"$cmake_version" &&
             echo $tag &&
-            echo $commit &&
-            if [[ $tag != "{commit}" ]]; then
-                echo "Tag mismatches version in CMakeLists.txt ( $tag vs {commit} )" && exit 1;
+            echo $version &&
+            if [[ $tag != "{version}" ]]; then
+                echo "Tag mismatches version in CMakeLists.txt ( $tag vs {version} )" && exit 1;
             fi
         # TODO AJM 2025/07/08: Once python package structure is standardized, add an else block for checking python package tags
         fi
     """)
     check_output(cmd)
-    print(f"Info: verified version in CMake, checked out {repo:<20} {commit:<20} under {outdir}.")
+    print(f"Info: verified version in CMake, checked out {repo:<20} {version:<20} under {outdir}.")
     return
 
 def load_packages(manifest_path, load_pymodules=False):
@@ -73,8 +73,31 @@ def load_packages(manifest_path, load_pymodules=False):
         pkgs += dunedaq_pymodules
     if not pkgs:
         print("Error: No packages found in manifest.")
-        sys.exit(20)
+        exit(20)
     return pkgs
+
+def get_checkout_token(name, branch, commit, version, source, check_tag=False):
+    if branch and (commit or re.search(r"\d+\.\d+\.\d+", version)):
+        print(textwrap.dedent(f"""\n
+            Error: {name} uses a fixed commit/tag in the manifest. Can't override with a branch.
+            Perhaps you need a manifest labeled 'develop'?
+        """))
+        exit(30)
+    if version == 'develop' and check_tag:
+        print(textwrap.dedent(f"""\n
+            Error: You requested to check tags ('-c'), but the manifest specifies the develop branch of {name}.
+            Perhaps you need a manifest labeled 'vX.Y.Z'?
+        """))
+        exit(31)
+    if branch:
+        return branch
+    if commit:
+        return commit
+    if re.search(r"\d+\.\d+\.\d+", version):
+        # Python package versions don't start with "v" in the release manifest; 
+        # add it here for consistency in checkout tokens
+        return f"v{version}" if source else version
+    return "develop"
 
 
 if __name__ == "__main__":
@@ -110,31 +133,34 @@ if __name__ == "__main__":
             commit = pkg.get("commit")
             version = pkg.get("version")
             source = pkg.get("source")
-            is_pymodule = True if source else False
-            if is_pymodule:
-                version = f"v{version}"
+            is_pymodule = bool(source)
+            token = get_checkout_token(name, args.branch, commit, version, source, args.check_tag)
             if args.check_tag:
-                checkout_tag(name, version, args.output_path, is_pymodule)
+                checkout_tag(name, token, args.output_path, is_pymodule)
             else:
-                token = args.branch or commit or (version if re.match(r"v\d+\.\d+\.\d+", version) else "develop")
                 if name == 'daq-cmake' and args.branch:
                     checkout_commit(name, version, args.output_path, is_success_required=False)
-                elif args.branch and (commit or re.match(r"v\d+\.\d+\.\d+", version)):
-                    print(f"\nError: {name} uses a fixed commit/tag in the manifest. Can't override with a branch.")
-                    sys.exit(30)
                 else:
                     checkout_commit(name, token, args.output_path) 
     elif args.package is not None:
-        pkg_entry = next((pkg for pkg in pkgs if pkg["name"] == args.package), None)
+        pkg_entry = next((pkg for pkg in pkgs if pkg.get("name") == args.package), None)
         if not pkg_entry:
             print(f"Error: {args.package} not found in {args.input_manifest}")
-            sys.exit(21)
-        version = args.branch or pkg_entry["version"]
-        commit = args.branch or pkg_entry["commit"] or pkg_entry["version"] or "develop"
+            print(f"       Note that you must pass -m or --pymodules to load python packages.")
+            exit(21)
+        name = pkg_entry.get("name")
+        if name == "elisa-client-api":
+            name = "elisa_client_api"
+        commit = pkg_entry.get("commit")
+        version = pkg_entry.get("version")
+        source = pkg_entry.get("source")
+        is_pymodule = bool(source)
+        token = get_checkout_token(name, args.branch, commit, version, source, args.check_tag)
+        print('TOKEN:', token)
         if args.check_tag:
-            checkout_tag(args.package, version, args.output_path)
+            checkout_tag(args.package, token, args.output_path, is_pymodule)
         else:
-            checkout_commit(args.package, commit, args.output_path)
+            checkout_commit(args.package, token, args.output_path)
     else:
-        print('Error: please specify "-a" or "-b <pkg>" option.')
+        print('Error: please specify "-a" or "-p <pkg>" option.')
         exit(22)
