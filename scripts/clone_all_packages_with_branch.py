6ff1d91ff61e19e4dac83f10e65abbed71ec62f7
John Freeman <jcfree@daq.fnal.gov>
2025-08-13 16:23:19 -0500

    JCF: various improvements to clone_all_packages_with_branch.py; treat pymodules on equal footing with C++ packages, exit with error if github token not found, and use more argument checks

--- scripts/clone_all_packages_with_branch.py
+++ scripts/clone_all_packages_with_branch.py
@@ -1,13 +1,14 @@
 import argparse
 import os
 import requests
+import sys
 
 from spack.dr_tools import get_packages
 
 ORG = "DUNE-DAQ"
 
 THIS_DIR=os.path.dirname( os.path.realpath(__file__) )
-PACKAGE_GROUPS = ["coredaq", "fddaq", "nddaq"]
+PACKAGE_GROUPS = ["coredaq", "fddaq", "pymodules"]
 RELEASE_TYPE = "develop"
 
 
@@ -16,7 +17,7 @@ def main():
 
     if GITHUB_TOKEN is None:
         print("GITHUB_TOKEN environment variable is not set.")
-        return
+        sys.exit(1)
 
     headers = {
         "Accept": "application/vnd.github+json",
@@ -24,17 +25,27 @@ def main():
     }
     parser = argparse.ArgumentParser(description="Get all repository in the DUNE-DAQ org which have a specific branch.")
     parser.add_argument("--branch", help="If set, only return packages from this branch", required=True)
-    parser.add_argument("--py-modules", help="only do python repository, else do only c++", action="store_true")
     parser.add_argument("--verbose", action="store_true", help="Print more information about the packages")
-    parser.add_argument("--package-group", choices=PACKAGE_GROUPS, default="coredaq", help="Package group to check. Default is 'coredaq'.")
+    parser.add_argument("--package-group", choices=PACKAGE_GROUPS, help="Package group to check. Can be 'coredaq', 'fddaq', or 'pymodules'")
     args = parser.parse_args()
     branch = args.branch
     verbose = args.verbose
-    py = args.py_modules
     package_group = args.package_group
 
+    if package_group not in ["coredaq", "fddaq", "pymodules"]:
+        print(f"ERROR: package group \"{package_group}\" not known")
+        sys.exit(2)
+
     repos_to_clone = []
-    yaml_filename = "{}/../configs/{}/{}-{}/release.yaml".format(THIS_DIR, package_group, package_group, RELEASE_TYPE)
+
+    # Account for the fact that for historical reasons the DUNE DAQ Python packages are listed in
+    # the release.yaml's in the fddaq directory
+
+    if package_group != "pymodules":
+        yaml_filename = "{}/../configs/{}/{}-{}/release.yaml".format(THIS_DIR, package_group, package_group, RELEASE_TYPE)
+    else:
+        yaml_filename = "{}/../configs/fddaq/fddaq-{}/release.yaml".format(THIS_DIR, RELEASE_TYPE)
+
     print(yaml_filename)
     if not os.path.exists(yaml_filename):
         print(f"YAML file {yaml_filename} does not exist.")
@@ -42,7 +53,7 @@ def main():
     if verbose:
         print(f"Checking {yaml_filename} for branch {branch} in package group {package_group}")
 
-    for repo in get_packages(yaml_file=yaml_filename, package_group=package_group if not py else "pymodules"):
+    for repo in get_packages(yaml_file=yaml_filename, package_group=package_group):
 
         branch_url = f"https://api.github.com/repos/{ORG}/{repo}/branches/{branch}"
         r = requests.get(branch_url, headers=headers)
