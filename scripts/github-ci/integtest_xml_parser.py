b55b18778d69b2b829b5b6b05a35df7a01a1d3ce
Andrew Mogan <amogan@fnal.gov>
2026-01-08 11:51:24 -0600

    Refactor xml parsing logic
    - Move parsing from other modules into this XML parser
    - Remove unnecessary arguments, functions, etc.
    - Reduce coupling

--- scripts/github-ci/integtest_xml_parser.py
+++ scripts/github-ci/integtest_xml_parser.py
@@ -3,40 +3,65 @@ import sys
 import re
 import argparse
 import html
-from pathlib import Path
 import xml.etree.ElementTree as ET
+from pathlib import Path
+from dataclasses import dataclass, field
+
+@dataclass
+class TestCaseResult:
+    name: str
+    status: str
+    failure_message: str = ''
+
+@dataclass
+class IntegrationTestSummary:
+    passed: int = 0
+    failed: int = 0
+    skipped: int = 0
+    errors: int = 0
+    total: int = 0
+    repo_results: dict[str, list[TestCaseResult]] = field(default_factory=dict)
+
+    def to_dict(self):
+        return {
+            "passed": self.passed,
+            "failed": self.failed,
+            "skipped": self.skipped,
+            "errors": self.errors,
+            "total": self.total,
+            "repo_results": self.repo_results
+        }
 
 class JUnitXMLParser:
-    def __init__(self, input_directory=None, input_file=None, output_filename="pytest_summary_table.md"):
-        self.input_directory = input_directory
-        self.input_file = input_file
-        self.output_filename = output_filename
+    def __init__(self, input_directory: str='', input_file: str=''):
+        self.input_directory = Path(input_directory) if Path(input_directory).is_dir() else None
+        self.input_file = Path(input_file) if Path(input_file).is_file() else None
         self.test_results = []
+        self.summary = IntegrationTestSummary()
 
-    def get_xml_files(self, directory, pattern):
-        if not os.path.isdir(directory):
-            raise FileNotFoundError(f"Error: {directory} is not a valid directory.")
+        if not self.input_directory and not self.input_file:
+            raise FileNotFoundError("No valid input file or directory specified.")
 
-        path = Path(directory)
-        xml_files = path.rglob(pattern)
+    def get_xml_files(self, pattern="*.xml"):
+        xml_files = self.input_directory.rglob(pattern)
         if not xml_files:
-            raise FileNotFoundError(f"Error: No xml files found in {directory}.")
+            raise FileNotFoundError(f"Error: No xml files found in {self.input_directory}.")
         return xml_files
 
-    def get_package_and_pytest_name_from_file(self, file_path):
-        file_name = os.path.basename(file_path)
-        # Junit xml files should be structured as <package_name>_<pytest_name>_results.xml
-        package_and_pytest_name = file_name.replace('_results.xml', '')
-        package_name, pytest_name = package_and_pytest_name.split("_", 1)
-        return package_name, pytest_name
+    # Junit xml file names should be structured as <package_name>_<pytest_name>_results.xml
+    def get_package_name(self, file):
+        return file.stem.split('_')[0]
 
-    def parse_junit_xml(self, file_path):
+    def get_pytest_name(self, file):
+        return file.stem.replace('_results', '').split('_', 1)[1]
+
+    def parse_xml_file(self, file_path):
         tree = ET.parse(file_path)
         root = tree.getroot()
 
-        package_name, pytest_name = self.get_package_and_pytest_name_from_file(file_path)
+        package_name = self.get_package_name(file_path)
+        pytest_name  = self.get_pytest_name(file_path)
 
-        results = []
         for testcase in root.findall(".//testcase"):
             test_name = testcase.get("name").split("[")[0]
             result = "passed"
@@ -50,14 +75,26 @@ class JUnitXMLParser:
             elif testcase.find("skipped") is not None:
                 result = "skipped"
 
-            results.append({
+            self.test_results.append({
                 "package_name": package_name,
                 "pytest_name": pytest_name,
                 "test_name": test_name,
                 "result": result,
                 "failure_message": failure_message
             })
-        return results
+
+    def summarize_failures(self):
+        failure_summary = ''
+        failed_line_pattern = r"\n>\s+(.*?)\n"
+
+        for result in self.test_results:
+            if not result.get('failure_message'):
+                continue
+            failure_summary += f"\n\t\t  *{result['test_name']}* failed"
+            failed_line_match = re.findall(failed_line_pattern, result['failure_message'])
+            if failed_line_match:
+                failure_summary += f" while checking \n\t\t`{failed_line_match[0]}`\n"
+        return failure_summary
 
     def which_emoji(self, test_status):
         emoji_map = {
@@ -68,31 +105,36 @@ class JUnitXMLParser:
         }
         return emoji_map.get(test_status, ':question:')
 
-    def format_markdown_row(self, test):
-        emoji = self.which_emoji(test['result'])
-        return f"| {test['test_name']} | {emoji} {test['result']} |\n"
+    def format_markdown_row(self, testname, result):
+        emoji = self.which_emoji(result)
+        return f"| {testname} | {emoji} {result} |\n"
 
-    def generate_markdown_table(self):
-        with open(self.output_filename, 'w') as f:
+    def generate_markdown_table(self, output_filename: str="pytest_summary_table.md"):
+        with open(output_filename, 'w') as f:
             for idx, result in enumerate(self.test_results):
                 if not result: continue
-                f.write(f"# {result[0]['package_name']} {result[0]['pytest_name']} Results\n")
+                f.write(f"# {result['package_name']} {result['pytest_name']} Results\n")
                 f.write("| Test Case | Status |\n")
                 f.write("|-----------|--------|\n")
-                f.writelines(self.format_markdown_row(test) for test in result)
+                f.writelines(self.format_markdown_row(result['pytest_name'], result['result']))
 
-        if not os.path.exists(self.output_filename):
-            raise FileNotFoundError(f"There was a problem writing the output markdown file: {self.output_filename}")
+        self.prepend_totals(output_filename)
 
-        print(f"Markdown summary generated at {self.output_filename}")
+        if not Path(output_filename).is_file():
+            raise FileNotFoundError(f"There was a problem writing the output markdown file: {output_filename}")
 
-    def prepend_test_summary(self):
+        print(f"Markdown summary generated at {output_filename}")
+
+    def prepend_totals(self, markdown_filename="pytest_summary_table.md"):
         num_passed = 0
         num_failed = 0
         num_skipped = 0
         num_errors = 0
         total_tests = 0
-        with open(self.output_filename, 'r') as ifile:
+        if not Path(markdown_filename).is_file():
+            raise FileNotFoundError(f"No markdown file named {markdown_filename} found.\n \
+                                      Make sure to generate the markdown file using 'this.generate_markdown_table()'.")
+        with open(markdown_filename, 'r') as ifile:
             original_lines = ifile.readlines()
             for line in original_lines:
                 print(line)
@@ -120,42 +162,62 @@ class JUnitXMLParser:
            {num_skipped} were skipped {self.which_emoji('skipped')}, and
            {num_errors} had errors {self.which_emoji('error')} which prevented the test from completing.\n"""
         new_lines = [summary] + original_lines
-        with open(self.output_filename, 'w') as ofile:
+        with open(markdown_filename, 'w') as ofile:
             ofile.writelines(new_lines)
 
-    def parse(self):
-        if self.input_directory and self.input_file:
-            print(f"Error: You must specify either an input directory or a specific file, not both.")
-            exit(1)
+    def parse_integration_test_summary(self, pytest_summary):
+        summary = IntegrationTestSummary()
 
-        if self.input_directory:
-            if not os.path.isdir(self.input_directory):
-                raise FileNotFoundError(f"Error: {self.input_directory} is not a valid directory.")
+        with open(pytest_summary, "r") as f:
+            num_header_lines = 5
+            lines = [line.strip() for line in f if line.strip()]
+            header_lines = lines[:num_header_lines]
+            results_lines = lines[num_header_lines:]
 
-            xml_files = self.get_xml_files(self.input_directory, "*.xml")
-            for file in xml_files:
-                self.test_results.append(self.parse_junit_xml(file))
+        numbers = [int(re.search(r'\d+', line).group()) for line in header_lines]
+        total, passed, failed, skipped, errors = numbers
+        summary.total = total
+        summary.passed = passed
+        summary.failed = failed
+        summary.skipped = skipped
+        summary.errors = errors
+        # Parse test results line by line
+        i = 0
+        while i < len(results_lines):
+            line = results_lines[i]
+            if line.startswith("#"):
+                # Each section represents a new package
+                section_title = re.sub(r"^#+ ", "", line)
+                summary.repo_results[section_title] = []
+                i += 3  # Skip table header and separator
 
-        elif self.input_file:
-            if not os.path.isfile(self.input_file):
-                raise FileNotFoundError(f"Error: Input file {self.input_file} is invalid.")
-            self.test_results.append(self.parse_junit_xml(self.input_file))
+                # Read test rows until next heading or end of list
+                while i < len(results_lines) and not results_lines[i].startswith("#"):
+                    row = results_lines[i]
+                    if "|" in row:
+                        columns = [c.strip() for c in row.strip("|").split("|")]
+                        if len(columns) == 2:
+                            test_name, status_col = columns
+                            status = "passed" if "passed" in status_col else "failed" if "failed" in status_col else "skipped" if "skipped" in status_col else "error"
+                            #summary["results"][section_title].append({
+                            #summary.repo_results[section_title].append({
+                            #    "name": test_name,
+                            #    "status": status
+                            #})
+                            test_result = TestCaseResult(name=test_name, status=status)
+                            summary.repo_results[section_title].append(test_result)
+                    i += 1
+            else:
+                i += 1
 
-        else:
-            raise RuntimeError(f"Error: No input file or directory specified. Exiting...")
-
-        self.generate_markdown_table()
-        self.prepend_test_summary()
+        return summary
 
 def main():
     parser = argparse.ArgumentParser(description="Parse a JUnit XML file and extract test case results.")
-    parser.add_argument("--input-directory", "-d",
+    parser.add_argument("--input-directory", "-d", type=str, default='',
                         help="Path to the directory containing junit xml files.")
-    parser.add_argument("--input-file", "-i",
+    parser.add_argument("--input-file", "-i", type=str, default='',
                         help="Path to a single JUnit XML file. Cannot be used in conjunction with --input-directory.")
-    parser.add_argument("--output-markdown-file", "-o", 
-                        default="pytest_summary_table.md",
-                        help="Name of the output file containing the markdown summary table. Default: ./pytest_summary_table.md")
 
     args = parser.parse_args()
 
@@ -163,16 +225,23 @@ def main():
         parser.print_usage()
         exit(1)
 
-    if args.input_directory and args.input_file:
-        print(f"Error: You must specify either an input directory or a specific file, not both.")
-        exit(1)
-
-    parser = JUnitXMLParser(
+    integtest_parser = JUnitXMLParser(
         input_directory=args.input_directory,
         input_file=args.input_file,
-        output_filename=args.output_markdown_file
     )
-    parser.parse()
+
+    if integtest_parser.input_directory:
+        xml_files = integtest_parser.get_xml_files()
+        for file in xml_files:
+            integtest_parser.parse_xml_file(file)
+
+    elif integtest_parser.input_file:
+        integtest_parser.parse_xml_file(integtest_parser.input_file)
+
+    integtest_parser.generate_markdown_table()
+    #integtest_parser.prepend_totals()
+    summary = integtest_parser.parse_integration_test_summary('pytest_summary_table.md')
+    print('SUMMARY:', summary)
 
 
 if __name__ == "__main__":
