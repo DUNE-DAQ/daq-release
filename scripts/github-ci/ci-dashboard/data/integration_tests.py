660dfdbb3b7c5fdc3875579f025a111ab4edd593
Andrew Mogan <amogan@fnal.gov>
2026-01-22 14:45:10 -0600

    Load and combine integration test summaries from json

--- scripts/github-ci/ci-dashboard/data/integration_tests.py
+++ scripts/github-ci/ci-dashboard/data/integration_tests.py
@@ -5,7 +5,8 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[4]))
 from parsers.junit_xml import JUnitXMLParser
 
-def load_integration_test_data(json_file: str):
-    with open(json_file) as f:
-        integration_test_data = json.load(f)
-    return integration_test_data
+def load_integration_test_data(core_json_file: str, extended_json_file: str):
+    core_summary = IntegrationTestSummary.from_json_file(core_json_file)
+    extended_summary = IntegrationTestSummary.from_json_file(extended_json_file)
+    combined_summary = IntegrationTestSummary.combined(core_summary, extended_summary)
+    return combined_summary
