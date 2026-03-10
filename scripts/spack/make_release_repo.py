33fba8747384c5813d1584cac753ac3999a1a57b
Andrew Mogan <amogan@fnal.gov>
2026-03-10 11:57:33 -0500

    Generate umbrellas using UmbrellaPackage

--- scripts/spack/make_release_repo.py
+++ scripts/spack/make_release_repo.py
@@ -39,17 +39,23 @@ def load_release_data(file: str) -> dict:
 
     return release_data
 
+@dataclass(frozen=True)
+class ReleaseContext:
+    release_name: str
+    core_release: str
+    full_umbrella: str
+    is_nightly_release: bool = False
+
+
 @dataclass
 class DAQPackage:
     name: str
     version: str
-    subset: str
-    release_name: str
-    full_umbrella: str
+    subset: str = None
     commit: str = None
     source: str = None
     variant: str = None
-
+    context: ReleaseContext = None
     contains_oks_file: bool = False
     cmake_dependencies: list[str] = field(default_factory=list)
 
@@ -66,11 +72,11 @@ class DAQPackage:
 
     @property
     def is_external(self) -> bool:
-        return self.kind in ["externals", "devtools", "systems"]
+        return self.subset in ["devtools", "externals", "systems"]
 
     @property
     def version_is_tag(self) -> bool:
-        return bool(re.search('\d+.\d+.\d+', self.version))
+        return bool(re.search('\d+.', self.version))
 
     @property
     def repo_url(self) -> str:
@@ -81,23 +87,27 @@ class DAQPackage:
     @property
     def possible_subset_qualifier(self) -> str:
         if self.subset == "externals":
-            return f', when="subset={self.full_umbrella}"'
+            return f', when="subset={self.context.full_umbrella}"'
         return ""
 
     @property
     def spec(self) -> str:
-        spec_version = self.version if self.version_is_tag else self.release_name
-        spec = f"{self.name}@{spec_version}"
+        spec_version = self.version if self.version_is_tag else self.context.release_name
 
-        # External packages
+        parts = [f"{self.name}@{spec_version}"]
+
         if self.variant:
-            spec += f" {self.variant}"
-            spec += f"{self.possible_subset_qualifier}"
-            return spec
+            parts.append(self.variant)
 
-        # DUNE-DAQ packages
-        spec += ' build_type={{build_type}}", when=f"build_type={{build_type}}'
-        return spec
+        base_spec = " ".join(parts)
+
+        if self.is_external:
+            return f'"{base_spec}"{self.possible_subset_qualifier}'
+
+        return (
+            f'"{base_spec} build_type={{build_type}}", '
+            f'when=f"build_type={{build_type}}"'
+        )
 
     # Update commit in both the DAQPackage object and the upstream DAQRelease.release_dict
     def set_commit(self, commit: str):
@@ -143,7 +153,7 @@ class DAQPackage:
         # Handle cases where the dependency name in CMakeLists.txt
         # doesn't match what Spack needs in its depends_on call
         self.cmake_dependencies = [
-            cmake_to_spack.get(dep, dep)
+            cmake_to_spack.get(dep, dep).lower()
             for dep in cmake_package_list
         ]
 
@@ -165,61 +175,43 @@ class DAQPackage:
 @dataclass
 class UmbrellaPackage:
     name: str
-    full_umbrella: str
-    core_release: str
+    is_dunedaq: bool
     template_path: str
     packages: list[DAQPackage]
+    context: ReleaseContext
 
     @property
     def template_file(self):
-        return Path(self.template_path / "packages" / self.name / "package.py")
+        return Path(self.template_path) / "packages" / self.name / "package.py"
 
     def render(self):
         content = self.template_file.read_text()
         
-        content = content.replace("XRELEASEX", self.release_dict["release"])
-        content = content.replace("XTARGETX", self.full_umbrella)
+        content = content.replace("XRELEASEX", self.context.release_name)
+        content = content.replace("XTARGETX", self.context.full_umbrella)
+
+        indent = "    "
+        deps = []
+        if self.is_dunedaq:
+            deps.append(f'{indent}for build_type in ["Debug", "RelWithDebInfo", "Release"]:')
+            indent += "    "
+            if self.name != "coredaq":
+                deps.append(
+                    f'{indent}depends_on(f"coredaq@{self.context.core_release} '
+                    f'subset={self.context.full_umbrella} build_type={{build_type}} +dev", '
+                    f'when=f"build_type={{build_type}} +dev")'
+                )
+                deps.append(
+                    f'{indent}depends_on(f"coredaq@{self.context.core_release} '
+                    f'subset={self.context.full_umbrella} build_type={{build_type}} ~dev", '
+                    f'when=f"build_type={{build_type}} ~dev")'
+                )
 
         for package in self.packages:
-            content += package.spec
+            if package.name == "dbe": continue
+            deps.append(f'{indent}depends_on(f{package.spec})')
 
-        print('UMBRELLA CONTENT:', content)
-        return content
-
-    def _dunedaq_dependencies(self, content):
-        content += '\n    for build_type in ["Debug", "RelWithDebInfo", "Release"]:'
-        if self.name != "coredaq":
-            content += f'\n        depends_on(f"coredaq@{self.core_release} subset={self.full_umbrella} build_type={{build_type}} +dev", when=f"build_type={{build_type}} +dev")'
-            content += f'\n        depends_on(f"coredaq@{self.core_release} subset={self.full_umbrella} build_type={{build_type}} ~dev", when=f"build_type={{build_type}} ~dev")'
-        for package in self.packages:
-            if package.name == "dbe":
-                continue
-            if package.name.startswith("py-"):
-                iver = idep["version"]
-                lines += f'\n        depends_on(f"{iname}@{iver}")'
-            else:
-                # Nightlies
-                if "daq" not in self.rdict["release"]:
-                    iver = self.rdict["release"]
-                lines += f'\n        depends_on(f"{iname}@{iver} build_type={{build_type}}", when=f"build_type={{build_type}}")'
-        lines += '\n'
-        return content
-
-    def _external_dependencies(self, content):
-        possible_subset_qualifier=""
-        if self.name == 'externals':
-            possible_subset_qualifier=f', when="subset={self.full_umbrella}"'
-
-        # now add additional deps:
-        print('Generating externals:', self.name)
-        print(f"{ipkg} packages: {self.packages}")
-        for package in self.packages:
-            if package.variant:
-                content += f'\n    depends_on("{package.name}@{package.version} {package.variant}"{possible_subset_qualifier})'
-            else:
-                content += f'\n    depends_on("{package.name}@{package.version}"{possible_subset_qualifier})'
-
-        return content
+        return content + "\n".join(deps) + "\n"
 
 
 class DAQRelease:
@@ -242,6 +234,7 @@ class DAQRelease:
         self.overwrite_branch = overwrite_branch
         self.overwrite_daq_cmake = overwrite_daq_cmake
         self.core_release = core_release
+        self.context = self.get_context()
 
         self.dunedaq_packages = self._load_dunedaq_packages()
 
@@ -275,9 +268,15 @@ class DAQRelease:
     def is_nightly_release(self):
         return "daq" not in self.release_dict["release"]
 
+    def get_context(self):
+        return ReleaseContext(
+            release_name=self.release_name,
+            core_release=self.core_release,
+            full_umbrella=self.full_umbrella,
+            is_nightly_release=self.is_nightly_release,
+        )
+
     def _load_packages(self, subset, source=None):
-        print("Attempting to load", subset, "with source", source)
-        print(f"{subset} exists?", self.release_dict.get(subset))
         return [
             DAQPackage(
                 _raw = entry,
@@ -287,8 +286,7 @@ class DAQRelease:
                 source=entry.get("source"),
                 variant=entry.get("variant"),
                 subset=subset,
-                release_name=self.release_name,
-                full_umbrella=self.full_umbrella,
+                context=self.context
             )
             for entry in self.release_dict.get(subset, [])
             if source is None or entry.get("source") == source
@@ -298,26 +296,14 @@ class DAQRelease:
         return self._load_packages(self.release_type) + self._load_packages("pymodules", source="github_DUNE-DAQ")
 
     def update_package_metadata(self):
-        #daq_packages = self._load_packages(self.release_type)
         for package in self.dunedaq_packages:
             package.get_git_info(self.update_hashes)
-        return
 
     def write_release_yaml(self):
         output_file = Path(f"{self.repo_dir}/{self.release_dict['release']}.yaml")
         with output_file.open("w") as outfile:
             outfile.write('---\n')
             yaml.dump(self.release_dict, outfile, Dumper=MyDumper, default_flow_style=False, sort_keys=False)
-        return
-
-    def generate_depends_on_list(self, cmake_package_list):
-        depends_on_list = ""
-        for idep in cmake_package_list:
-            # Special cases where find_package call in CMakeLists is not sufficient
-            if idep in cmake_to_spack:
-                idep = cmake_to_spack[idep]
-            depends_on_list += f'\n    depends_on("{idep}")'
-        return depends_on_list
 
     def generate_repo_file(self):
         with Path(f'{self.repo_dir}/repo.yaml').open('w') as f:
@@ -364,71 +350,28 @@ class DAQRelease:
 
             self._write_package_file(package.name, content)
 
+    def _generate_umbrella(self, name: str, is_dunedaq: bool):
+        umbrella = UmbrellaPackage(
+            name=name,
+            template_path=self.template_path,
+            packages=self._load_packages(name),
+            context=self.context,
+            is_dunedaq=is_dunedaq
+        )
+        content = umbrella.render()
+        self._write_package_file(name, content)
+
     def generate_external_umbrella_package(self):
-        for ipkg in ["devtools", "externals", "systems"]:
-            content = self._load_template(ipkg)
-            
-            content = content.replace("XRELEASEX", self.release_dict["release"])
-            content = content.replace("XTARGETX", self.full_umbrella)
+        for subset in ["devtools", "externals", "systems"]:
+            self._generate_umbrella(subset, is_dunedaq=False)
 
-            possible_subset_qualifier=""
-            if ipkg == 'externals':
-                possible_subset_qualifier=f', when="subset={self.full_umbrella}"'
+    def generate_daq_umbrella_package(self):
+        self._generate_umbrella(self.release_type, is_dunedaq=True)
 
-            # now add additional deps:
-            print('Generating externals:', ipkg)
-            packages = self._load_packages(ipkg)
-            print(f"{ipkg} packages: {packages}")
-            for package in packages:
-                if package.variant:
-                    content += f'\n    depends_on("{package.name}@{package.version} {package.variant}"{possible_subset_qualifier})'
-                else:
-                    content += f'\n    depends_on("{package.name}@{package.version}"{possible_subset_qualifier})'
-
-            self._write_package_file(ipkg, content)
-
-        return
-
-    def generate_daq_umbrella_package(self, repo_path, template_dir):
-        repo_dir = os.path.join(repo_path, "spack-repo", "packages")
-        template_dir = os.path.join(template_dir, "packages")
-        ipkg = self.release_type
-        itemp = os.path.join(template_dir, ipkg, 'package.py')
-        if not os.path.exists(itemp):
-            print(f"Error: template file {itemp} is not found!")
-            return
-        with open(itemp, 'r') as f:
-            lines = f.read()
-            lines = lines.replace("XRELEASEX", self.rdict["release"])
-            lines = lines.replace("XTARGETX", self.full_umbrella)
-
-        # now add additional deps:
-        lines += '\n    for build_type in ["Debug", "RelWithDebInfo", "Release"]:'
-        if self.release_type != "coredaq":
-            lines += f'\n        depends_on(f"coredaq@{self.rdict["core_release"]} subset={self.full_umbrella} build_type={{build_type}} +dev", when=f"build_type={{build_type}} +dev")'
-            lines += f'\n        depends_on(f"coredaq@{self.rdict["core_release"]} subset={self.full_umbrella} build_type={{build_type}} ~dev", when=f"build_type={{build_type}} ~dev")'
-        for idep in self.rdict[ipkg]:
-            iname = idep["name"]
-            iver = idep["version"]
-            if iname == "dbe":
-                continue
-            if iname.startswith("py-"):
-                iver = idep["version"]
-                lines += f'\n        depends_on(f"{iname}@{iver}")'
-            else:
-                # Nightlies
-                if "daq" not in self.rdict["release"]:
-                    iver = self.rdict["release"]
-                lines += f'\n        depends_on(f"{iname}@{iver} build_type={{build_type}}", when=f"build_type={{build_type}}")'
-        lines += '\n'
-
-        ipkg_dir = os.path.join(repo_dir, ipkg)
-        os.makedirs(ipkg_dir)
-        ipkgpy = os.path.join(ipkg_dir, "package.py")
-        with open(ipkgpy, 'w') as o:
-            o.write(lines)
-            print(f"Info: package.py has been written at {ipkgpy}.")
-        return
+    def generate_umbrella_packages(self):
+        if self.release_type == "coredaq":
+            self.generate_external_umbrella_package()
+        self.generate_daq_umbrella_package()
 
     def generate_repo(self):
         self.repo_dir.mkdir(parents=True)
@@ -436,10 +379,7 @@ class DAQRelease:
         self.write_release_yaml()
         self.generate_repo_file()
         self.generate_daq_packages()
-        if self.release_type == "coredaq":
-            self.generate_external_umbrella_package()
-        #else:
-        #    self.generate_daq_umbrella_package()
+        self.generate_umbrella_packages()
         return
 
     def generate_pypi_manifest(self, output_file):
@@ -525,28 +465,13 @@ if __name__ == "__main__":
         args.overwrite_daq_cmake
     )
 
-    #if args.update_hash:
-    #    daq_release.update_hashes()
-
     if args.pypi_manifest:
-        #os.makedirs(args.output_path, exist_ok=True)
-        #outfile = os.path.join(args.output_path, 'pypi_manifest.sh')
         daq_release.generate_pypi_manifest()
     elif args.pyvenv_requirements:
-        #os.makedirs(args.output_path, exist_ok=True)
-        #outfile = os.path.join(args.output_path, 'pyvenv_requirements.txt')
         daq_release.generate_pyvenv_requirements()
-    #elif args.check_branch:
-    #    tmp_dir = tempfile.mkdtemp()
-    #    daq_release.copy_release_yaml(tmp_dir, True)
-    #    shutil.rmtree(tmp_dir)
     else:
         if not args.release_name:
             raise ValueError("--release-name is required for generating a release repo.")
         if not args.template_path:
             raise ValueError("--template-path is required for generating a release repo.")
         daq_release.generate_repo()
-        #daq_release.generate_repo(args.update_hash, args.core_release)
-        #daq_release.generate_repo(args.output_path, args.template_path,
-        #                          args.update_hash, args.release_name,
-        #                          args.core_release)
